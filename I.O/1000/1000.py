7944126f73c6f20fde4b6eab05537f4ed782eb9d
JiheeSeon <starlover27@naver.com>
2021-01-10 13:59:26 +0900

    organizing java branch

--- I.O/1000/1000.py
+++
@@ -1,3 +0,0 @@
-[A,B] = input().split(" ")
-res = int(A) + int(B)
-print(res)
--- I.O/10950/10950.kt
+++
@@ -1,6 +0,0 @@
-import java.util.Scanner;
-fun main(args: Array<String>) = with(Scanner(System.`in`)){
-    var T = nextInt() //Testcase number
-    for i in (1..T)
-        println(nextInt() + nextInt())
-}
