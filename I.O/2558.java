a9fa8a8ae3bbc6b1c52c20a51219802bb717c783
JiheeSeon <starlover27@naver.com>
2021-01-12 13:29:34 +0900

    comment added for 2558 in java

--- I.O/2558.java
+++ I.O/2558.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 
 /* BufferedReader, BufferedWriter 사용해보기 */
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { //BufferedReader 사용할 때 IOException 꼭 걸어줘야함
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(Integer.toString(Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine())));
+        bw.write(Integer.toString(Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine()))); //형변환 안 시켜줘도 됨
         bw.flush();
         bw.close();
     }
