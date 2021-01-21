97f56ba49e77b7009f11834a193d834b91fa12be
JiheeSeon <starlover27@naver.com>
2021-01-21 14:15:11 +0900

    2741 in java

--- I.O/11022.java
+++ I.O/11022.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-class Main {
+class Main11022 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
---
+++ I.O/2741.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Main2741 {
+    public static void main(String[] args) throws IOException {
+        IntStream ins = IntStream.rangeClosed(1, Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine()));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(ins.mapToObj(Integer::toString).collect(Collectors.joining("\n")));
+        bw.flush();
+        bw.close();
+    }
+}
