fea71dc70a42502cf872a4f4018a6ab0facff76c
Moren <mathlover27@gmail.com>
2021-01-27 11:37:20 +0900

    string manipulation :: 11654 in java

---
+++ String/11654.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+class Main11654{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.read());
+//        System.out.println(br.readLine().charAt(0));
+    }
+}
