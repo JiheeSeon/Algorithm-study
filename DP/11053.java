2d7ad496a25490605de8e510b220367692055500
Moren <mathlover27@gmail.com>
2021-02-09 14:13:28 +0900

    DP :: 11053 in java

--- DP/11053.java
+++ DP/11053.java
@@ -1,34 +1,56 @@
 import java.io.*;
 import java.util.regex.Pattern;
 
-class Main11053{
-    static long[] memo;
-    static long[] amount;
+class Main11053 {
+    static final int MEMO = 0;
+    static final int MAX = 1;
+    static long[] originalInput;
+    static long[] maxLengthOfIncrementalSequenceEndsWithIndex;
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        memo = new long[N + 1];
-        amount = new long[N + 1];
 
-        amount = Pattern.compile(" ").splitAsStream("0 "+br.readLine()).limit(N + 1).mapToLong(Long::parseLong).toArray();
+        maxLengthOfIncrementalSequenceEndsWithIndex = new long[N + 1];
+        originalInput = Pattern.compile(" ").splitAsStream("0 " + br.readLine()).mapToLong(Long::parseLong).limit(N + 1).toArray();
 
-        for(int i = 1 ; i <= N ; i++) {
-            memo[i] = 1;
-            solveWithDP(i);
-        }
+        for (int i = 1; i <= N; i++)
+            checkLongestIncrementalSequence(i);
 
-        bw.write(Long.toString(memo[N]));
+        long result = 0;
+
+        for(long k: maxLengthOfIncrementalSequenceEndsWithIndex)
+            if (result < k)
+                result = k;
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Long.toString(result));
         bw.flush();
         bw.close();
     }
 
-    static void solveWithDP(int n){
-        for(int j = 1; j <= n ; j++){
-            if(memo[n] < memo[j] + 1 && amount[j] < amount[n])
-                memo[n] = memo[j] + 1;
+    static void checkLongestIncrementalSequence(int n) {
+        /*testcase 8   -- 10 25 20 50 30 23 40 70*/
+        int maxIndex;
+        long maxLength;
+
+        switch (n) {
+            case 1 -> maxLengthOfIncrementalSequenceEndsWithIndex[n] = 1;
+            case 2 -> maxLengthOfIncrementalSequenceEndsWithIndex[n] = originalInput[n] > originalInput[1] ? 2 : 1;
+            default -> {
+                maxIndex = 0;
+                maxLength = 1;
+
+                for (int k = n - 1; k >= 1; k--) {
+                    if (originalInput[k] < originalInput[n]
+                            && originalInput[maxIndex] <= originalInput[k]) {
+                        maxIndex = k;
+                        maxLength = Math.max(maxLength, maxLengthOfIncrementalSequenceEndsWithIndex[maxIndex]);
+                    }
+                }
+                maxLengthOfIncrementalSequenceEndsWithIndex[n] = maxIndex == 0 ? 1 : 1 + maxLength;
+            }
         }
     }
 }
