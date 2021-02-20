4fe3a64018b6232f14f4c34bb5942f4b22558cc7
JiheeSeon <starlover27@naver.com>
2021-02-20 23:53:26 +0900

    Graph Resolve :: 2667 in java

---
+++ Graph/Resolve/2667.java
@@ -0,0 +1,76 @@
+package Resolve;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+class Main2667Resolve {
+    static int mapSize;
+    static int aptNum = 0;
+
+    static int[][] visited;
+    static int[][] graph;
+    static int[] res;
+
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder stb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        mapSize = Integer.parseInt(br.readLine());
+        visited = new int[mapSize][mapSize];
+        graph = new int[mapSize][mapSize];
+
+        int i, j;
+
+        for (i = 0; i < mapSize; i++)
+            graph[i] = processInput(mapSize);
+
+        for (i = 0; i < mapSize; i++) {
+            for (j = 0; j < mapSize; j++) {
+                /*마킹된 단지거나(이미 방문), 유효하지 않은 노드의 경우*/
+                if (visited[i][j] == 0 && graph[i][j] != 0)
+                    dfs(i, j, ++aptNum);
+            }
+        }
+
+        res = new int[aptNum];
+
+        for (i = 0; i < mapSize; i++) {
+            for (j = 0; j < mapSize; j++) {
+                if (visited[i][j] != 0)
+                    res[visited[i][j] - 1]++;
+            }
+        }
+        Arrays.sort(res);
+
+        stb.append(aptNum).append("\n");
+
+        for (i = 0; i < aptNum; i++)
+            if (res[i] != 0)
+                stb.append(res[i]).append("\n");
+
+        bw.write(stb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static int[] processInput(int limitSize) throws IOException {
+        return Pattern.compile("").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).limit(limitSize).toArray();
+    }
+
+    static void dfs(int x, int y, int apt) {
+        /* 밑에서 재귀로 현재 위치에서 갈 수 있는 건 다 호출 > 앞에서 막아줘야 함 */
+        if (x < 0 || y < 0 || x >= mapSize || y >= mapSize) return;
+        if (visited[x][y] != 0) return;
+        if (graph[x][y] == 0) return;
+
+        visited[x][y] = apt;
+
+        for (int i = 0; i < 4; i++)
+            dfs(x + dx[i], y + dy[i], apt);
+    }
+}
