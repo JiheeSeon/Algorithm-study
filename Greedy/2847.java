ac3c059e487a557d248d44fac772b5b2643fc0da
JiheeSeon <starlover27@naver.com>
2021-02-22 11:16:08 +0900

    Greedy :: 2847 in java

---
+++ Greedy/2847.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+class Main2847{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int level = getIntegerInput();
+        int[] score = new int[level];
+        for (int i = 0; i < level; i++)
+            score[i] = getIntegerInput();
+
+        int result = 0;
+
+        for (int i = 2; i <= score.length; i++) {
+            if (score[score.length - i] >= score[score.length - i + 1]) {
+                result += score[score.length - i] - score[score.length - i + 1] + 1;
+                score[score.length - i] = score[score.length - i + 1] - 1;
+            }
+        }
+
+        bw.write(Integer.toString(result));
+        bw.flush();
+        bw.close();
+    }
+    static int getIntegerInput() throws IOException{
+        return Integer.parseInt(br.readLine());
+    }
+}
