36e92988df9e3d793ac8c02f111bf6fa913cfde3
JiheeSeon <starlover27@naver.com>
2021-02-23 16:08:38 +0900

    Greedy :: 2839 in greedy

--- Greedy/2839.java
+++ Greedy/2839.java
@@ -1,8 +1,42 @@
 import java.io.*;
 
-class Main2839{
+class Main2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int i, j;
+
+        int[] greedy = new int[N + 1];
+
+        int[] temp = {0, -1, -1, 1, -1, 1, 2, -1, 2, 3, 2};
+
+        int candidate;
+
+        if (N % 5 == 0) greedy[N] = N / 5;
+        else {
+            for (i = 1; i <= N; i++) {
+                if (i < temp.length) greedy[i] = temp[i];
+                else {
+                    for (j = 1; j < i / 3; j++) {
+                        if (i % 5 == 0) greedy[i] = i / 5;
+                        else {
+
+                            if (greedy[i - 3 * j] != -1) {
+                                candidate = greedy[3 * j] + greedy[i - 3 * j];
+
+                                if (greedy[i] == 0 || greedy[i] > candidate) {
+                                    greedy[i] = candidate;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        bw.write(Integer.toString(greedy[N]));
+        bw.flush();
+        bw.close();
     }
 }
