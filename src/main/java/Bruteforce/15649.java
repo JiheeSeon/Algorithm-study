dee7f5ce7522fb066c9cab46cb473cf18f58178b
JiheeSeon <starlover27@naver.com>
2021-02-26 16:33:39 +0900

    Bruteforce[Backtracking] :: 15649

---
+++ src/main/java/Bruteforce/15649.java
@@ -0,0 +1,44 @@
+package Bruteforce;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+class Main15649{
+    static StringBuilder stb = new StringBuilder();
+    static StringBuilder stbRec = new StringBuilder();
+    static int maxN, count;
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] input = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        maxN = input[0]; count = input[1];
+
+        recursive(1);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(stb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void recursive(int digit){
+        if (digit > count){
+            stb.append(stbRec.toString()).append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= maxN; i++){
+            if (!stack.isEmpty() && stack.contains(i)) continue;
+
+            stbRec.append(i).append(" ");
+            stack.push(i);
+            recursive(digit + 1);
+            stack.pop();
+
+            stbRec.setLength((digit - 1) * 2);
+        }
+    }
+}
