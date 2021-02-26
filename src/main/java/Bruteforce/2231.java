1b94c74d50b02f9cd91fcce714fae4158f024a51
JiheeSeon <starlover27@naver.com>
2021-02-26 12:38:14 +0900

    2231, first solu

--- src/main/java/Bruteforce/2231.java
+++ src/main/java/Bruteforce/2231.java
@@ -2,56 +2,32 @@ package Bruteforce;
 
 import java.io.*;
 
-class Main2231{
-    static int[] res;
+class Main2231 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String inputStr = br.readLine();
-        int inputN = Integer.parseInt(inputStr);
+        int N = Integer.parseInt(br.readLine());
+        final int MAX_AVAILABLE = 1000054;
 
-        int length = inputStr.length();
-        int[] toMultiply = new int[length];
+        int[] constructor = new int[MAX_AVAILABLE];
 
-        int i;
+        int quotient, decomposeSum;
 
-        /* 1. 생성자가 몇자리 수로 이루어져있는지 계산
-        입력으로 받은 수의 자리수보다 작은 자리수로 이룰 수 있는 결과의 최대값을 벤치마크로 계산
-        ex. 1034 -> 999 + 9 + 9 + 9
-        */
-        int benchmark = 9 * length; // a + b + c => 9 + 9 + 9 = 9 * length
+        for (int i = 1; i <= N; i++) {
+            quotient = decomposeSum = i;
 
-        for (i = 0; i < length; i++)
-            benchmark += 9 * Math.pow(10, i);
+            while(true){
+                decomposeSum += quotient % 10;
+                if (quotient / 10 == 0) break;
+                quotient /= 10;
+            }
 
-        if (inputN <= benchmark) length--; // 생성자의 length fix
-
-        for (i = length; i > 1; i--){
-            toMultiply[i - 1] = 1 << (i-1) | 1;
-        }
-        toMultiply[0] = 2;
-
-        res = new int[length];
-        int temp = inputN;
-
-        for (i = 0; i < length; i++){
-            res[i] = temp / toMultiply[i];
+            if (decomposeSum > MAX_AVAILABLE) break;
+            if (constructor[decomposeSum] == 0) constructor[decomposeSum] = i;
         }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(" ");
+        bw.write(Integer.toString(constructor[N]));
         bw.flush();
         bw.close();
-    }
-
-    static void recursive(int N, int idx){ // 현재 남은 수, 몇번째 자리,
-        if (idx == 0) {
-            res[idx] = N;
-//            return 1;
-        }
-        else{
-            for (int i = 0; i < 10; i++){
-
-            }
-        }
     }
 }
