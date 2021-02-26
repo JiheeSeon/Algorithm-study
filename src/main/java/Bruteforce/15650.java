a6277f2a6c4f957aba27c7afb261a04e151dd5b2
JiheeSeon <starlover27@naver.com>
2021-02-26 17:06:05 +0900

    Bruteforce[Backtracking] :: 15650, basic example

---
+++ src/main/java/Bruteforce/15650.java
@@ -0,0 +1,38 @@
+package Bruteforce;
+
+import java.io.*;
+import java.util.regex.Pattern;
+
+class Main15650 {
+    static int N, M;
+    static StringBuilder stbForResult = new StringBuilder();
+    static StringBuilder stbForBranch = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        N = input[0];
+        M = input[1];
+
+        recursive(1, 0);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(stbForResult.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    static void recursive(int depth, int previous) {
+        if (depth > M){
+            stbForResult.append(stbForBranch.toString()).append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (i <= previous) continue;
+            stbForBranch.append(i).append(" ");
+            recursive(depth + 1, i);
+            stbForBranch.setLength(2 * (depth - 1));
+        }
+    }
+}
