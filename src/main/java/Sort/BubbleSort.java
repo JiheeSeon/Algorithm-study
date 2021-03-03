10f3ee12ffcdd8b1e294c7099ebf056b7d1ebbec
JiheeSeon <starlover27@naver.com>
2021-03-03 12:52:53 +0900

    Sort :: bubblesort implementation in java

---
+++ src/main/java/Sort/BubbleSort.java
@@ -0,0 +1,35 @@
+package Sort;
+
+import java.io.*;
+import java.util.regex.Pattern;
+
+class BubbleSort{
+    static int[] input;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+
+        bubbleSort();
+
+        StringBuilder stb = new StringBuilder();
+        for (int j : input)
+            stb.append(j).append(" ");
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(stb.toString());
+        bw.flush();
+        bw.close();
+    }
+    static void bubbleSort(){
+        int temp;
+        for (int endPoint = input.length - 1; endPoint > 0; endPoint--){
+            for (int i = 0; i < endPoint; i++){
+                if (input[i] > input[i+1]){ //swap
+                    temp = input[i];
+                    input[i] = input[i+1];
+                    input[i+1] = temp;
+                }
+            }
+        }
+    }
+}
