25dbd06cf3c9c1fec6bf94d61b5b9cb6c1d9dcb8
JiheeSeon <starlover27@naver.com>
2021-03-10 15:28:31 +0900

    Programmers  12916 count py :: basic sol

---
+++ src/main/java/String/Programmers/lv1/12916-pyCounter.java
@@ -0,0 +1,28 @@
+package String.Programmers.lv1;
+
+import java.io.*;
+import java.util.Locale;
+
+class Main12916 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(Boolean.toString(solution(s)));
+        bw.flush();
+        bw.close();
+    }
+
+    static boolean solution(String s) {
+        int numOfP = 0, numOfY = 0;
+        s = s.toLowerCase();
+
+        for (int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'p') numOfP++;
+            else if(s.charAt(i) == 'y') numOfY++;
+        }
+
+        return numOfP == numOfY;
+    }
+}
