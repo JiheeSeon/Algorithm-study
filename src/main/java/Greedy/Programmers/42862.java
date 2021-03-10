e90e30079290ffd2a2c410f16f52bbd00d62616d
JiheeSeon <starlover27@naver.com>
2021-03-10 15:04:37 +0900

    Programmers 12901  체육복 빌려주기:: I/O added

--- src/main/java/Greedy/Programmers/42862.java
+++ src/main/java/Greedy/Programmers/42862.java
@@ -1,13 +1,19 @@
 package Greedy.Programmers;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 class Main42862{
     public static void main(String[] args) throws IOException {
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = 5;
+        int[] lost = {2,4};
+        int[] reserve = {1, 3, 5};
+        bw.write(Integer.toString(solution(N, lost, reserve)));
+        bw.flush();
+        bw.close();
     }
     static int solution(int n, int[] lost, int[] reserve) {
         int[] spare = new int[n + 1];
