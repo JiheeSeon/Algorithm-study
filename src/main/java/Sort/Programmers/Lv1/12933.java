d497dddf6937b5d78362b924fed4d801a473f942
JiheeSeon <starlover27@naver.com>
2021-03-12 04:09:01 +0900

    Programmers 12933 - stream hell..

---
+++ src/main/java/Sort/Programmers/Lv1/12933.java
@@ -0,0 +1,22 @@
+package Sort.Programmers.Lv1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+class Main12933{
+    static long solution(long n) {
+        /* Failed approach
+        Integer[] ns = (Integer[]) Pattern.compile("").splitAsStream(Long.toString(n)).mapToInt(Integer::parseInt).boxed().toArray();
+        Integer[] ns = (Integer[]) Pattern.compile("").splitAsStream(Long.toString(n)).map(Integer::parseInt).toArray();
+        Integer[] ns =  (Integer[]) Pattern.compile("").splitAsStream(Long.toString(n)).mapToInt(Integer::parseInt).mapToObj(o->(Integer)o).toArray();
+        * */
+        int[] ns =  Pattern.compile("").splitAsStream(Long.toString(n)).mapToInt(Integer::parseInt).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::valueOf).toArray();
+        return Long.parseLong(Arrays.stream(ns).mapToObj(Integer::toString).collect(Collectors.joining()));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(123024958L));
+    }
+}
