6576b16e05846d33278c966113dd353a1a78b963
JiheeSeon <starlover27@naver.com>
2021-03-18 00:36:01 +0900

    Backtracking / Acmicpc 15664 N&M(10) & 15666 N&M(12)

---
+++ src/main/java/Backtrack/Acmicpc/15666-N과M(12).java
@@ -0,0 +1,48 @@
+package Backtrack.Acmicpc;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+class Main15666{
+    static int N, maxDigit;
+    static int[] numbers;
+    static LinkedHashSet<String> set = new LinkedHashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        N = input[0]; maxDigit = input[1];
+        numbers = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+
+        int[] prev = new int[maxDigit];
+        Arrays.fill(prev, -1);
+
+        backtrack(1, prev);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder stb = new StringBuilder();
+        for(String s : set){stb.append(s);}
+        bw.write(stb.toString());
+        bw.flush();
+        bw.close();
+    }
+    static void backtrack(int currentDigit, int[] prevIndices){
+        for (int i = (currentDigit == 1) ? 0 : prevIndices[currentDigit - 2]; i < N; i++){
+            prevIndices[currentDigit - 1] = i;
+
+            if(currentDigit == maxDigit) putOnSet(prevIndices);
+            else backtrack(currentDigit + 1, prevIndices);
+
+            prevIndices[currentDigit - 1] = -1;
+        }
+    }
+    static void putOnSet(int[] prevIndices){
+        StringBuilder localStb = new StringBuilder();
+        for(int i : prevIndices){localStb.append(numbers[i]).append(" ");}
+        localStb.append("\n");
+
+        set.add(localStb.toString());
+    }
+}
