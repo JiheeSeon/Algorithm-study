90f643e865fcfa4d1bef185df8955969234e06f1
JiheeSeon <starlover27@naver.com>
2021-03-24 12:29:28 +0900

    Backtracking / Acmicpc 10597 solved

--- src/main/java/Backtrack/Acmicpc/10597-순열장난.java
+++ src/main/java/Backtrack/Acmicpc/10597-순열장난.java
@@ -1,75 +1,47 @@
 package Backtrack.Acmicpc;
 
 import java.io.*;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
-
-//1111112211111111111111
 
 class Main10597{
     static StringBuilder stb =  new StringBuilder();
     static String input;
-    static int minN, maxN;
+    static int N;
     static boolean flagToBreak = false;
-
-    static int maxValue=0;
-
-    static LinkedList<Integer> previous = new LinkedList<>();
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         input = br.readLine();
-        setRangeOfN();
+        N = (input.length() < 10) ? input.length(): 9 + ((input.length() - 9) / 2);
+        visited = new boolean[N + 1];
+        visited[0] = true;
 
         backtrack(0);
 
         System.out.println(stb.toString());
     }
     static void backtrack(int currDigit){
-        int maxDist = 1; int currCandidate;
-        if (currDigit != input.length() -1) maxDist = 2;
+        int currentIntValue;
+        int end = currDigit == input.length() - 1 ? 1 : 2;
 
-        for (int i = 1; i <= maxDist; i++){
-            if (flagToBreak) break;
-            currCandidate = Integer.parseInt(input.substring(currDigit, currDigit + i));
-            if (currCandidate == 0 || currCandidate > maxN) break;
-
-            int existedMax = maxValue;
-            maxValue = Math.max(existedMax, currCandidate);
-            previous.add(currCandidate);
+        for (int i = 1; i <= end; i++){
+            currentIntValue = Integer.parseInt(input.substring(currDigit, currDigit + i));
+            if (currentIntValue > N) break;
+            if (visited[currentIntValue]) continue;
+            
+            stb.append(currentIntValue).append(" ");
+            visited[currentIntValue] = true;
 
             if (currDigit + i >= input.length()){
-                if (previous.size() == maxValue){
-                    stb.append(previous.stream().map(s -> Integer.toString(s)).collect(Collectors.joining(" ")));
-                    System.out.println("maxValue = " + maxValue);
-                    System.out.println("previous.size() = " + previous.size());
-                    flagToBreak = true;
-                }
+                flagToBreak = true;
+                break;
             } else backtrack(currDigit + i);
 
-            previous.removeLast();
-            if (existedMax != maxValue) maxValue = existedMax;
-        }
-    }
-    static void setRangeOfN(){
-        char previous = ' '; char current = ' ';
-        int currentIntValue = -1;
-        StringBuilder localStb = new StringBuilder();
+            if (flagToBreak) break;
+            if (currentIntValue >= 10) stb.setLength(stb.length() - 3);
+            else stb.setLength(stb.length() - 2);
 
-        for(int i = 0; i < input.length(); i++){
-            current = input.charAt(i);
-            if (current == '0'){
-                minN = Math.max(minN, Integer.parseInt(localStb.append(previous).append(current).toString()));
-                localStb.setLength(0);
-            } else {
-                currentIntValue = Integer.parseInt(String.valueOf(current));
-                minN = Math.max(minN, currentIntValue);
-            }
-            previous = current;
-        }
-        maxN = Math.max(minN, input.length());
-
-        System.out.println("minN = " + minN);
-        System.out.println("maxN = " + maxN);
+            visited[currentIntValue] = false;
+;        }
     }
 }
