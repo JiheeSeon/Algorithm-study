4cb5bd670567321ca11446fb085abb9eb1e3505c
JiheeSeon <starlover27@naver.com>
2021-03-25 10:58:53 +0900

    Backtracking / N&M 7 기본형 + 후보군 입력받기

---
+++ src/main/java/Backtrack/Acmicpc/resolveNM/Main7.java
@@ -0,0 +1,42 @@
+package Backtrack.Acmicpc.resolveNM;
+
+import java.io.*;
+import java.util.regex.Pattern;
+import java.util.*;
+
+class Main7{
+    /* 입력 받은 자연수 중 M개를 고른 수열 + 같은 수를 여러번 골라도 됨 */
+
+    static int candidateN; // 올 수 있는 경우의 가짓수
+    static int maxDepth; // 수열의 길이, 수열의 각 자리를 고르고 재귀호출 -> 호출 depth
+    static int[] choiceIdx; // 각 자리마다 선택한 값에 해당되는 index를 저장한 배열
+    static int[] numbers; // 실제 선택의 대상이 될 후보군 (사용자 입력)
+    static StringBuilder stb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        candidateN = input[0]; maxDepth = input[1];
+        choiceIdx = new int[maxDepth];
+
+        numbers = Pattern.compile(" ").splitAsStream(br.readLine()).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+        Arrays.fill(choiceIdx, -1);
+
+        backtrack(0);
+        System.out.println(stb.toString());
+    }
+    static void backtrack(int depth){
+        if (depth == maxDepth){
+            for (int i = 0; i < maxDepth; i++) stb.append(numbers[choiceIdx[i]]).append(" ");
+            stb.append("\n");
+            return;
+        }
+        for(int candidateIdx = 0; candidateIdx < candidateN; candidateIdx++){
+            // 같은 수를 여러번 골라도 됨 -> 기본형 (모든 index 중 하나)
+            choiceIdx[depth] = candidateIdx;
+            backtrack(depth + 1);
+            choiceIdx[depth] = -1;
+        }
+    }
+}
