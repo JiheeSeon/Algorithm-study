304da545704ff892f2e2ca1e899a5e0de3d129ac
JiheeSeon <starlover27@naver.com>
2021-03-31 20:26:04 +0900

    2 pointer / 2118 두개의 탑, fixed, but more fixes needed

--- src/main/java/Two_Pointer/2118-두개의탑.java
+++ src/main/java/Two_Pointer/2118-두개의탑.java
@@ -21,21 +21,41 @@ class Main2118{
         int start = 0, end = 0;
         int sum = 0;
 
+        long allSum = pos;
         long benchmark = pos / 2;
         long approximate = Long.MAX_VALUE;
+        int startIdx = -1, endIdx = -1;
+
+        long previousSum = -1;
+        long won;
 
         while(true){
             if (sum == benchmark){
                 approximate = benchmark;
+                startIdx = start; endIdx = end;
                 break;
             } else if (sum > benchmark){
-                if (Math.abs(sum - benchmark)
-                        < Math.abs(benchmark - approximate)) approximate = sum;
-                sum -= clockwiseCoordinate[start++];
-            } else if (end == N) break;
-            else sum += clockwiseCoordinate[end++];
-        }
+                won = (Math.abs(sum - benchmark) > Math.abs(previousSum - benchmark)) ? previousSum : sum;
 
-        System.out.println(approximate);
+                if (Math.abs(won - benchmark)
+                        < Math.abs(approximate - benchmark)){
+                    startIdx = start; endIdx = end;
+                    approximate = sum;
+                }
+
+                sum -= clockwiseCoordinate[start++];
+                previousSum = sum;
+            } else if (end == N) break;
+            else{
+                sum += clockwiseCoordinate[end++];
+                previousSum = sum;
+            }
+        }
+        System.out.println("startIdx = " + startIdx);
+        System.out.println("endIdx = " + endIdx);
+        System.out.println("approximate = " + approximate);
+        System.out.println("benchmark = " + benchmark);
+
+        System.out.println(Math.min(approximate, Math.abs(allSum - approximate)));
     }
 }
