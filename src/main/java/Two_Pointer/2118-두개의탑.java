ddc4b07399885d27c034cce984f8d10690f5da1a
JiheeSeon <starlover27@naver.com>
2021-04-01 15:43:21 +0900

    2 pointer / 2118 두개의 탑, debugging

--- src/main/java/Two_Pointer/2118-두개의탑.java
+++ src/main/java/Two_Pointer/2118-두개의탑.java
@@ -4,58 +4,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// TESTCASE
+//5
+//1
+//3
+//4
+//9
+//7
+
 class Main2118{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int[] clockwiseCoordinate = new int[N];
+        int[] deltas = new int[N];
 
         int i; long pos = 0;
 
         for(i = 0; i < N; i++){
-            clockwiseCoordinate[i] = Integer.parseInt(br.readLine());
-            pos += clockwiseCoordinate[i];
+            deltas[i] = Integer.parseInt(br.readLine());
+            pos += deltas[i];
         }
 
+        long perimeter = pos; long benchmark = perimeter / 2;
         int start = 0, end = 0;
-        int sum = 0;
+        long sum = 0; long error = Long.MAX_VALUE;
+        long result = 0;
 
-        long allSum = pos;
-        long benchmark = pos / 2;
-        long approximate = Long.MAX_VALUE;
-        int startIdx = -1, endIdx = -1;
-
-        long previousSum = -1;
-        long won;
-
+        long currentError;
         while(true){
             if (sum == benchmark){
-                approximate = benchmark;
-                startIdx = start; endIdx = end;
+                error = 0;
+                result = sum;
                 break;
             } else if (sum > benchmark){
-                won = (Math.abs(sum - benchmark) > Math.abs(previousSum - benchmark)) ? previousSum : sum;
+                currentError = Math.abs(sum - benchmark) + Math.abs((perimeter - sum) - benchmark);
 
-                if (Math.abs(won - benchmark)
-                        < Math.abs(approximate - benchmark)){
-                    startIdx = start; endIdx = end;
-                    approximate = sum;
+                if (currentError < error){
+                    error = currentError;
+                    result = Math.min(sum, perimeter - sum);
                 }
-
-                sum -= clockwiseCoordinate[start++];
-                previousSum = sum;
+                sum -= deltas[start++];
             } else if (end == N) break;
-            else{
-                sum += clockwiseCoordinate[end++];
-                previousSum = sum;
-            }
+            else sum += deltas[end++];
         }
-        System.out.println("startIdx = " + startIdx);
-        System.out.println("endIdx = " + endIdx);
-        System.out.println("approximate = " + approximate);
-        System.out.println("benchmark = " + benchmark);
 
-        System.out.println(Math.min(approximate, Math.abs(allSum - approximate)));
+        System.out.println(result);
     }
 }
