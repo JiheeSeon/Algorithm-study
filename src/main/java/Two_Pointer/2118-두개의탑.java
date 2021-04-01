24229c291c735985b2407931d92e239792cfb7ba
JiheeSeon <starlover27@naver.com>
2021-04-01 16:21:36 +0900

    2 pointer / 2118 두개의 답

--- src/main/java/Two_Pointer/2118-두개의탑.java
+++ src/main/java/Two_Pointer/2118-두개의탑.java
@@ -4,13 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// TESTCASE
+// TESTCASE 1
 //5
 //1
 //3
 //4
 //9
 //7
+
+// TESTCASE 2
+//1
+//100
+//50
+//70
+//3
+//1
 
 class Main2118{
     public static void main(String[] args) throws IOException {
@@ -27,25 +35,16 @@ class Main2118{
 
         long perimeter = pos; long benchmark = perimeter / 2;
         int start = 0, end = 0;
-        long sum = 0; long error = Long.MAX_VALUE;
+        long sum = 0;
         long result = 0;
 
-        long currentError;
         while(true){
-            if (sum == benchmark){
-                error = 0;
-                result = sum;
-                break;
-            } else if (sum > benchmark){
-                currentError = Math.abs(sum - benchmark) + Math.abs((perimeter - sum) - benchmark);
-
-                if (currentError < error){
-                    error = currentError;
-                    result = Math.min(sum, perimeter - sum);
-                }
-                sum -= deltas[start++];
-            } else if (end == N) break;
+            if (sum == benchmark){ result = sum; break;}
+            else if (sum > benchmark) sum -= deltas[start++];
+            else if (end == N) break;
             else sum += deltas[end++];
+
+            result = Math.max(result, Math.min(sum, perimeter - sum));
         }
 
         System.out.println(result);
