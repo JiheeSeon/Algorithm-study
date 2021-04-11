b0d18cab5aafbbbd2c0793347723f5872247fcb8
JiheeSeon <starlover27@naver.com>
2021-04-11 16:41:14 +0900

    Binary Search Basic Example

---
+++ src/main/java/BinarySearch/IterationExample.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class IterationExample {
+    public int binarysearch(int[] arr, int R) {
+        int T = 0;
+        int high = arr.length - 1;
+        int low = 0;
+
+        int mid;
+
+        while(high >= low) {
+            mid = (high + low) / 2;
+            T++;
+            if(arr[mid] == R) return T;
+            else if (arr[mid] < R) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return T;
+    }
+}
