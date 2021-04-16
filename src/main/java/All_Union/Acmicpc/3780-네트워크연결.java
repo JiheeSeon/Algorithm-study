820a936c777b8ff5280d37394d2ddaf043417c29
JiheeSeon <starlover27@naver.com>
2021-04-16 13:49:07 +0900

    Union Find / 네트워크 연결 / snapshot2

--- src/main/java/All_Union/Acmicpc/3780-네트워크연결.java
+++ src/main/java/All_Union/Acmicpc/3780-네트워크연결.java
@@ -11,6 +11,8 @@ class Main3780{
     /*문제 요약
     A의 루트 노드를 B 트리의 자식 or 루트 노드의 자식으로 넣어버리기 (B에 A 흡수)
     */
+
+    static Integer tmp = 0;
     static Map<Enterprise, Enterprise> parent;
     static Enterprise[] enterprises;
 
@@ -37,22 +39,16 @@ class Main3780{
 
             while(!input[0].equals("O")) {
                 if(input[0].equals("E")){
-                    System.out.println("finding ");
-
                     arbitraryIdx = Integer.parseInt(input[1]);
                     find(arbitraryIdx, enterprises[arbitraryIdx].dst);
-                    stb.append(enterprises[arbitraryIdx].dst).append("\n");
+                    enterprises[arbitraryIdx].dst = tmp;
+                    stb.append(tmp).append("\n");
                 } else{
                     centerIdx = Integer.parseInt(input[1]);
                     arbitraryIdx = Integer.parseInt(input[2]);
-
-                    System.out.println("union "+ centerIdx + " " +arbitraryIdx);
                     union(centerIdx, arbitraryIdx);
                 }
                 input = br.readLine().split(" ");
-
-                System.out.println(Arrays.toString(enterprises));
-                System.out.println();
             }
         }
         System.out.println(stb);
@@ -65,25 +61,25 @@ class Main3780{
         if(!pA.equals(pB)){ // pB가 무조건 PA를 흡수
             parent.put(pA, pB);
             pA.dst += (enterprises[b].dst + Math.abs(a - b) % 1000); // 거리 update
-            System.out.println(enterprises[b].dst);
         }
     }
 
     static Enterprise find(int x, Integer x_updatedD) {
-        if(enterprises[x].equals(parent.get(enterprises[x])))
+        if(enterprises[x].equals(parent.get(enterprises[x]))) {
+            tmp = x_updatedD;
             return enterprises[x];
+        }
         else{
-            // x의 distance에 자신의 부모까지의 거리를 추가해줌
+            // x의 distance에 자신의 부모까지의 거리를 추가
             x_updatedD += parent.get(enterprises[x]).dst;
             Enterprise pE = find(parent.get(enterprises[x]).idx, x_updatedD);
-            System.out.println(" " + x_updatedD);
             parent.put(enterprises[x], pE);
             return pE;
         }
     }
 
     static private class Enterprise{
-        Integer dst = 0; // 나의 직속 부모와의 거리
+        Integer dst = 0;
         final int idx;
 
         public Enterprise(int idx) {
@@ -96,3 +92,22 @@ class Main3780{
         }
     }
 }
+/*
+1
+13
+I 1 2
+E 2
+E 1
+I 3 2
+I 4 5
+I 5 3
+E 3
+I 9 8
+I 10 9
+I 13 9
+E 13
+I 2 10
+E 5
+E 4
+O
+*/
