397f5bde700da7f12ba63fa5d702d6b846c8b3c9
JiheeSeon <starlover27@naver.com>
2021-04-30 15:22:34 +0900

    Tree / 15900 / 나무탈출 sol 3 / Timeout

--- src/main/java/Tree/나무탈출_15900.java
+++ src/main/java/Tree/나무탈출_15900.java
@@ -3,57 +3,47 @@ package Tree;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+/*Idea -> depth 를 더한다*/
 class 나무탈출_15900 {
-    static int[] parent;
+    static Integer[] parent;
     static ArrayList<Integer>[] graph;
     static Set<Integer> alreadyRegistered = new HashSet<>();
-    static LinkedList<Integer> leafLength = new LinkedList<>();
+    static ArrayList<LeafInfo> lf = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        parent = new int[N + 1];
-        graph = new ArrayList[N + 1];
-        for(int i = 1; i < graph.length; i++)
+        parent = new Integer[N + 1];
+        graph = new ArrayList[N +1];
+        for (int i = 0; i < N + 1; i++) {
             graph[i] = new ArrayList<>();
+        }
 
         int[] tmp;
-        for(int i = 0; i < N - 1 ; i++){
+        for(int i = 0; i < N - 1 ; i++) {
             tmp = strToIntArray(br.readLine());
             graph[tmp[0]].add(tmp[1]);
             graph[tmp[1]].add(tmp[0]);
         }
-
         setParent(1, 1, 0);
-        System.out.println((leafLength.stream().mapToInt(x -> (int) x).sum())% 2 == 0 ? "No": "Yes");
 
-//        Set<Integer> leaf = IntStream.rangeClosed(1, N).boxed().collect(Collectors.toCollection(HashSet::new));
-//        for(int i = 1; i <= N; i++){
-//            leaf.remove(parent[i]);
-//        }
-//
-//        int totalLength = 0;
-//        for(int i : leaf)
-//            totalLength += getPathLength(i, 0);
-
-//        System.out.println(leafLength);
-//        System.out.println(totalLength % 2 == 0 ? "No" : "Yes");
+        int res = 0;
+        for(LeafInfo linfo : lf){
+            res += linfo.depth;
+        }
+//        System.out.println(res);
+        System.out.println(res % 2 == 0 ? "No" : "Yes");
     }
 
     static int[] strToIntArray(String s) {
         return Pattern.compile(" ").splitAsStream(s).mapToInt(Integer::parseInt).toArray();
     }
 
-    static void setParent(int now, int prev, int len){
+    static void setParent(int now, int prev, int depth){
 //        if(parent[now] != 0) return;
 
         parent[now] = prev;
@@ -62,16 +52,25 @@ class 나무탈출_15900 {
         int validLoop = 0;
         for (int i : graph[now]) {
             if(!alreadyRegistered.contains(i)){
-                setParent(i, now, len + 1);
+                setParent(i, now, depth + 1);
                 validLoop++;
             }
         }
 
-        if(validLoop == 0) leafLength.add(len);
+        if(validLoop == 0) lf.add(new LeafInfo(depth, now));
     }
 
     static int getPathLength(int now, int len){
         if(now == 1) return len;
         return getPathLength(parent[now], len + 1);
+    }
+
+    private static class LeafInfo{
+        int depth, number;
+
+        public LeafInfo(int depth, int number) {
+            this.depth = depth;
+            this.number = number;
+        }
     }
 }
