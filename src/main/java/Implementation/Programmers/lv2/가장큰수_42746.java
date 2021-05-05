d2e60a7c83c7baff2fcb0ed23f9d9ec6d7cee313
JiheeSeon <starlover27@naver.com>
2021-05-05 17:07:44 +0900

    프로그래머스 2 / 42746 가장 큰 수 / right, simple, sad solution

--- src/main/java/Implementation/Programmers/lv2/가장큰수_42746.java
+++ src/main/java/Implementation/Programmers/lv2/가장큰수_42746.java
@@ -4,35 +4,6 @@ import java.util.stream.*;
 import java.util.*;
 import java.util.stream.*;
 
-class Solution_Timeout {
-    String s = "";
-    int maxDepth = 1;
-
-    public String solution(int[] numbers) {
-        maxDepth = numbers.length;
-        backtrack(0, new StringBuilder(), new boolean[maxDepth], numbers);
-        return s;
-    }
-
-    void backtrack(int depth, StringBuilder stb, boolean[] check, int[] numbers){
-        if(depth == maxDepth){
-            if(stb.toString().compareTo(s) > 0)
-                s = stb.toString();
-            return;
-        }
-
-        for(int i = 0; i < maxDepth; i++){
-            if(check[i]) continue;
-
-            check[i] = true;
-            stb.append(numbers[i]);
-            backtrack(depth + 1, stb, check, numbers);
-            stb.setLength(stb.length() - (Integer.toString(numbers[i]).length()));
-            check[i] = false;
-        }
-    }
-}
-
 class 가장큰수_42746 {
     String s = "";
     int maxDepth = 1;
@@ -66,37 +37,10 @@ class 가장큰수_42746 {
     class StrComparator implements Comparator<String> {
         @Override
         public int compare(String a, String b) {
-            if(a.length() == b.length()) return -a.compareTo(b);
-            if(a.charAt(0) != b.charAt(0)) return -a.compareTo(b);
+            StringBuilder stb1 = new StringBuilder(a).append(b);
+            StringBuilder stb2 = new StringBuilder(b).append(a);
 
-            int aIdx = 0; int bIdx = 0;
-            StringBuilder stb = new StringBuilder();
-
-            while(aIdx < a.length() && bIdx < b.length()){
-                if(a.charAt(aIdx) > b.charAt(bIdx)) return -1;
-                else if(a.charAt(aIdx) < b.charAt(bIdx)) return 1;
-                else stb.append(a.charAt(aIdx));
-                aIdx++; bIdx++;
-            }
-            int delta = 0;
-            int toRet;
-
-            if(aIdx == a.length()){ // b가 더 길다 -> stb는 a용
-                while(aIdx < b.length()){
-                    stb.append(b.charAt(delta++));
-                    aIdx++;
-                }
-                toRet = -stb.toString().compareTo(b);
-                return toRet == 0 ? 1 : toRet;
-            }
-            else{ // a가 더 길다 -> stb는 b용
-                while(bIdx < a.length()){
-                    stb.append(a.charAt(delta++));
-                    bIdx++;
-                }
-                toRet = -a.compareTo(stb.toString());
-                return toRet == 0 ? -1 : toRet;
-            }
+            return -stb1.compareTo(stb2);
         }
     }
 }
