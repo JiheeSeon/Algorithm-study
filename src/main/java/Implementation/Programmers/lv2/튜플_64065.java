f5647135d2db55ce765bdc7821e69e50f2c89cb4
JiheeSeon <starlover27@naver.com>
2021-05-06 16:54:50 +0900

    프로그래머스 2 / 64065 튜플 / 문자열 regex / comment added

--- src/main/java/Implementation/Programmers/lv2/튜플_64065.java
+++ src/main/java/Implementation/Programmers/lv2/튜플_64065.java
@@ -7,14 +7,14 @@ class 튜플_64065 {
     public int[] solution(String s) {
         // input 문자열 처리
         String stripped = s.substring(1, s.length() - 1);
-        Pattern p = Pattern.compile("\\{[[0-9]*,]*[0-9]+\\}");
+        Pattern p = Pattern.compile("\\{[[0-9]*,]*[0-9]+\\}"); //숫자 또는 쉼표 중에 0개 이상 나온 후 숫자로 끝나야 함
         Matcher m = p.matcher(stripped);
 
-        int groupN = 0;
+        int groupN = 0; // groupCount() 메소드는 다른 것.
         String thisStr, original;
         int[] tmpArr;
         Map<Integer, int[]> map = new HashMap<>();
-        while(m.find()){
+        while(m.find()){ // 패턴이 일치하는 경우 true를 반환, 그 위치로 이동(여러개가 매칭되는 경우 반복 실행가능)
             original = m.group();
             thisStr = original.substring(1, original.length() - 1);
             tmpArr = Pattern.compile(",").splitAsStream(thisStr).mapToInt(Integer::parseInt).toArray();
