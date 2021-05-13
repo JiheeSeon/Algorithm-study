fbac4a46487035e22b4111e2d9c0ee8bf1a2016f
JiheeSeon <starlover27@naver.com>
2021-05-13 18:13:06 +0900

    프로그래머스 2 / 12985 예상대진표 / comment and other solution added

--- src/main/java/Implementation/Programmers/lv2/예상대진표_12985.java
+++ src/main/java/Implementation/Programmers/lv2/예상대진표_12985.java
@@ -2,6 +2,9 @@ package Implementation.Programmers.lv2;
 
 import java.util.*;
 
+// 풀이방법이 잘 생각이 안 나
+// 이진트리에서 parent 올라가는 경로를 저장해서 공통 부모의 하나 아래에서까지 경합 수 count.
+
 class 예상대진표_12985 {
     public int solution(int n, int a, int b){
         int min = n - 1 + Math.min(a, b);
@@ -20,11 +23,14 @@ class 예상대진표_12985 {
         }
 
         int currIdx = aPath.size();
-
         while(--currIdx >= 0)
             if(!aPath.get(currIdx).equals(bPath.get(currIdx))) break;
 
         return currIdx + 1;
+    }
+
+    public int solution_other(int n, int a, int b){
+        return Integer.toBinaryString((a-1)^(b-1)).length();
     }
 }
 class Main12985{
@@ -32,5 +38,7 @@ class Main12985{
         System.out.println(new 예상대진표_12985().solution(8, 4, 7));
         System.out.println(new 예상대진표_12985().solution(16, 1,  15));
 
+        System.out.println(new 예상대진표_12985().solution_other(8, 4, 7));
+        System.out.println(new 예상대진표_12985().solution_other(16, 1,  15));
     }
 }
