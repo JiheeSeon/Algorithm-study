db23b4720056e5bb79ef86cf72299fe27ab6d955
JiheeSeon <starlover27@naver.com>
2021-06-02 14:28:37 +0900

    BFS & DFS / 2644 촌수계산 / DFS 1

--- src/main/java/Graph/Archive/BDFS/촌수계산_2644.java
+++ src/main/java/Graph/Archive/BDFS/촌수계산_2644.java
@@ -56,6 +56,47 @@ class 촌수계산_2644_BFS {
     }
 }
 
+class 촌수계산_2644_DFS_1 {
+    int V, E, S, D;
+    int[] check;
+    ArrayList<Integer>[] graph;
+    int ans = -1;
+
+    public 촌수계산_2644_DFS_1(int V, int E, int S, int D, int[][] connection, int[] check){
+        this.V = V;
+        this.E = E;
+        this.S = S;
+        this.D = D;
+        this.check = check;
+
+        graph = new ArrayList[V];
+        for(int v = 0; v < V; v++)
+            graph[v] = new ArrayList<>();
+
+        for(int e = 0; e < E; e++){
+            graph[connection[e][0] - 1].add(connection[e][1] - 1);
+            graph[connection[e][1] - 1].add(connection[e][0] - 1);
+        }
+    }
+
+    void dfs(int now, int prev){
+        if(check[now] != 0) return;
+
+        if(now == S) check[now] = 0;
+        else check[now] = check[prev] + 1;
+
+        if(now == D) ans = check[now];
+
+        for(int next: graph[now]){
+            dfs(next, now);
+        }
+    }
+
+    int getAns(){
+        return ans;
+    }
+}
+
 
 class MainA2644 {
     public static void main(String[] args) throws IOException{
@@ -72,9 +113,13 @@ class MainA2644 {
             connection[e] = strToIntArr(br.readLine());
         }
 
-        촌수계산_2644_BFS s = new 촌수계산_2644_BFS(V, E, S, D, connection, check);
-        s.bfs();
-        System.out.println(s.getAns());
+        촌수계산_2644_BFS s_bfs = new 촌수계산_2644_BFS(V, E, S, D, connection, check);
+        s_bfs.bfs();
+        System.out.println(s_bfs.getAns());
+
+        촌수계산_2644_DFS_1 s_dfs_1 = new 촌수계산_2644_DFS_1(V, E, S, D, connection, new int[V]);
+        s_dfs_1.dfs(S, -1);
+        System.out.println(s_dfs_1.getAns());
     }
 
     static int[] strToIntArr(String s){
