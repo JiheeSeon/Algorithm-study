1e9961f2059df876ffb3a51b15747bf0b49a9550
JiheeSeon <starlover27@naver.com>
2021-06-03 16:16:13 +0900

    DFS / 2573 빙산 / 기본 sol : 그래프 세팅하고 연결요소의 개수 count

--- src/main/java/Graph/Archive/BDFS/빙산_2573.java
+++ src/main/java/Graph/Archive/BDFS/빙산_2573.java
@@ -1,72 +1,115 @@
 package Graph.Archive.BDFS;
 
 import java.io.*;
-import java.util.*;
 import java.util.regex.Pattern;
 
 /*
 2573 빙산
+주변에 갈 수 없는 길이 있을 때 빼야 함.
+그룹의 개수를 세는 건 dfs를 사용하든 bfs를 사용하든 상관 없음.
+값을 바꾸는걸 각각 진행하고 dfs로 연결요소 개수 카운트하면 될듯
+
 전부 다 녹을 때까지 2 덩어리 이상으로 분리되지 않은 경우 -> 0 출력
+=> 모두 다 녹았는데 덩어리로 쪼개지지 않은 경우
 */
+
 class Solution{
     int yHeight, xWidth;
-    int[][] graph;
+    int[][] oldGraph;
+    int[][] newGraph;
     boolean[][] check;
+    boolean zeroFlag;
 
     int[] dy = {-1, 1, 0, 0};
     int[] dx = {0, 0, -1, 1};
 
-    public Solution(int yHeight, int xWidth, int[][] graph){
+    public Solution(int yHeight, int xWidth, int[][] oldGraph){
         this.yHeight = yHeight;
         this.xWidth = xWidth;
-        this.graph = graph;
+        this.oldGraph = oldGraph;
+        newGraph = new int[yHeight][xWidth];
     }
 
     int getAns(){
-        int t = 0;
+        int t = 1;
         int groupN;
-        boolean stopFlag;
 
-        while(true){
-            groupN = 0;
-            check = new boolean[yHeight][xWidth];
-            stopFlag = true;
+        while(true) {
+            zeroFlag = true;
+
+            setNewGraph();
+            if(getGroupCnt() >= 2) return t;
+
+            backupNewGraphToOldGraph();
+            if(zeroFlag) return 0;
+
             t++;
-
-            for(int y = 0; y < yHeight; y++){
-                for(int x = 0; x < xWidth; x++){
-                    if(graph[y][x] != 0) stopFlag = false;
-
-                    if(!check[y][x] && graph[y][x] != 0){
-                        dfs(y, x);
-                        groupN++;
-                    }
-                }
-            }
-
-            System.out.println(t + " :: " + groupN);
-            display();
-
-            if(stopFlag) return 0;
-            if(groupN >= 2) return t;
         }
     }
 
+    void backupNewGraphToOldGraph() {
+        int toSubtract;
+
+        for (int y = 0; y < yHeight; y++) {
+            for(int x = 0; x < xWidth; x++){
+                oldGraph[y][x] = newGraph[y][x];
+                if(oldGraph[y][x] != 0) zeroFlag = false;
+            }
+        }
+    }
+
+    void setNewGraph() {
+        int toSubtract;
+        int nextY, nextX;
+
+        for (int y = 0; y < yHeight; y++) {
+            for (int x = 0; x < xWidth; x++) {
+                toSubtract = 0;
+
+                for(int i = 0; i < 4; i++){
+                    nextY = y + dy[i];
+                    nextX = x + dx[i];
+
+                    if(nextY < 0 || nextX < 0 || nextY >= yHeight || nextX >= xWidth) continue;
+                    if(oldGraph[y + dy[i]][x+dx[i]] == 0) toSubtract++;
+                }
+
+                newGraph[y][x] = Math.max(oldGraph[y][x] - toSubtract, 0);
+            }
+        }
+    }
+
+    int getGroupCnt(){
+        int groupN = 0;
+
+        check = new boolean[yHeight][xWidth];
+        for (int y = 0; y < yHeight; y++) {
+            for (int x = 0; x < xWidth; x++) {
+                if(!check[y][x] && newGraph[y][x] != 0){
+                    dfs(y, x);
+                    groupN++;
+                }
+            }
+        }
+        return groupN;
+    }
+
     void dfs(int y, int x){
-        if(y < 0 || x < 0 || y >= yHeight || x >= xWidth || check[y][x]
-                || (graph[y][x] = graph[y][x] >= 1 ? graph[y][x] - 1 : 0) == 0) return;
+        if(y < 0 || x < 0 || y >= yHeight || x >= xWidth || check[y][x] || newGraph[y][x] == 0)
+            return;
 
         check[y][x] = true;
 
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < 4; i++){
             dfs(y + dy[i], x + dx[i]);
+        }
     }
 
     void display(){
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder("\n");
         for(int y = 0; y < yHeight; y++){
             for(int x = 0; x < xWidth; x++){
-                stringBuilder.append(graph[y][x]).append(" ");
+                stringBuilder.append(newGraph[y][x]).append(" ");
             }
             stringBuilder.append("\n");
         }
