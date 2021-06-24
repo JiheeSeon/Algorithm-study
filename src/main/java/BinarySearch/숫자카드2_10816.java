edf984c2da1b43754f38a3a17f7e42b6e8827040
JiheeSeon <starlover27@naver.com>
2021-06-24 16:59:12 +0900

    Parametric Search / 10815 숫자카드2 / upper & lower bound algorithm

--- src/main/java/BinarySearch/숫자카드2_10816.java
+++ src/main/java/BinarySearch/숫자카드2_10816.java
@@ -4,6 +4,14 @@ import java.io.*;
 import java.util.regex.Pattern;
 import java.util.Arrays;
 
+/*
+Bound Algorithm
+lowerBound : 찾고자 하는 값 이상이 처음 나타나는 위치 리턴
+upperBound : target 값 보다 큰 값이 처음 나오는 위치 리턴
+
+[0 - N-1]이 아니라 [0 - N] 구간으로
+while loop도 low < high를 조건으로
+*/
 class 숫자카드2_10816{
     int N, M;
     long[] cards;
@@ -16,12 +24,57 @@ class 숫자카드2_10816{
         this.toCheck = toCheck;
     }
 
+    // 찾고자 하는 값 이상이 처음 나타나는 위치
+    int lowerBound(long target){
+//        System.out.println("\nstart lower bound call");
+
+        int low = 0; int high = N;
+        int mid;
+
+        while (low < high) {
+            mid = (low + high) / 2;
+//            System.out.println("low = " + low + " high = " + high + " mid = " + mid);
+
+            if(cards[mid] < target) low = mid + 1;
+            else high = mid;
+        }
+
+//        System.out.println("end lower bound call with low = " + low + " high = " + high + "\n");
+        return high;
+    }
+
+    // 특정 target값보다 큰 값이 처음 나오는 위치를 리턴
+    int upperBound(long target) {
+//        System.out.println("start upper bound call\n");
+
+        int low = 0; int high = N;
+        int mid;
+
+        while (low < high) {
+            mid = (low + high) / 2;
+
+//            System.out.println("low = " + low + " high = " + high + " mid = " + mid);
+
+            if(cards[mid] <= target) low = mid + 1;
+            else high = mid;
+        }
+
+//        System.out.println("end upper bound call with low = " + low + " high = " + high);
+        return high;
+    }
+
+    int binary_search_bound_algorithm(long target) {
+        int lb = lowerBound(target);
+        int ub = upperBound(target);
+
+//        System.out.println("lb = " + lb + " ub = " + ub);
+        return ub - lb;
+    }
+
     int binary_search_iteration(long target){
         int low = 0; int high = N - 1;
         int mid;
         int l = -1; int h = -1;
-
-        // 가장 최대로 target이 나온 것 - 가장 최소로 target이 나온 것
 
         while(low <= high){
             mid = (low + high) / 2;
@@ -72,7 +125,7 @@ class 숫자카드2_10816{
 
         StringBuilder stb = new StringBuilder();
         for(long target: toCheck){
-            stb.append(binary_search_iteration(target)).append(" ");
+            stb.append(binary_search_bound_algorithm(target)).append(" ");
         }
         return stb.toString();
     }
