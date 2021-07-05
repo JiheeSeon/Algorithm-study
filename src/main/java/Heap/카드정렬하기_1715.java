73a35b1ce427fbbc49a9d0814d2dd7d0ffd24104
JiheeSeon <starlover27@naver.com>
2021-07-05 14:15:54 +0900

    Heap / 1715 카드정렬하기 / right sol

--- src/main/java/Heap/카드정렬하기_1715.java
+++ src/main/java/Heap/카드정렬하기_1715.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 /*
-반례
+반례 #1
 10
 10
 10
@@ -20,8 +20,25 @@ import java.util.PriorityQueue;
 10
 10
 
-앞에꺼부터 더하는 게 안 통함.
+반례 #2
+6
+15
+30
+31
+32
+50
+54
+
+A + B + A + B + C <= A + B + C + D
+즉, A + B <= D 일 때만 기존의 방식이 통함. (앞에서부터 주루룩 더하는 거)
+
+수정한 방식은, 가장 작은 합이 나올 수 있는 것들의 합이 결론적인 최소합이 됨.
+이 때, 가장 작은 원소 2개를 더한 것이 sum의 최솟값이므로
+minHeap을 만들어, 매번 구한 합을 다시 힙에 넣음.
+
+heap 에 최종적으로 원소가 하나 남으면 더 이상 더할 쌍이 없는 것.
 */
+
 class 카드정렬하기_1715 {
     int N;
     PriorityQueue<Integer> minHeap;
@@ -32,21 +49,24 @@ class 카드정렬하기_1715 {
     }
 
     int solution() {
-        int now;
-        int summed = 0;
+        if(N == 1) return 0;
+
+        int now, next;
+        boolean firstFlag = true;
         int ret = 0;
 
-        while (!minHeap.isEmpty()) {
+        while (minHeap.size() >= 2) {
             now = minHeap.poll();
+            next = minHeap.poll();
 
-            // 첫 묶음일 경우
-            if(summed == 0){
-                summed = now;
+            if(firstFlag){
+                firstFlag = false;
+                ret += now + next;
+                minHeap.add(now + next);
                 continue;
             }
-
-            summed += now;
-            ret += summed;
+            minHeap.add(now + next);
+            ret += now + next;
         }
         return ret;
     }
