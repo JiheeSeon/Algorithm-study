3accc87209dc3433c35c210a056f9cb5758e99c5
JiheeSeon <starlover27@naver.com>
2021-07-05 14:57:51 +0900

    Heap / 7662 이중우선순위큐 / out of memory, 예상원인은 programmers 방식으로 해서

---
+++ src/main/java/Heap/이중우선순위큐_7662.java
@@ -0,0 +1,109 @@
+package Heap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class 이중우선순위큐_7662 {
+    int T;
+    int[] operationCounts;
+    String[][] operations;
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+
+    public 이중우선순위큐_7662(int[] operationCounts, String[][] operations) {
+        T = operationCounts.length;
+        this.operationCounts = operationCounts;
+        this.operations = operations;
+    }
+
+    String solution() {
+        StringBuilder stb = new StringBuilder();
+
+        boolean maxHeapFlag = true;
+        String[] operation;
+
+        while (--T >= 0) {
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+            for (int i = 0; i < operationCounts[T]; i++) {
+                operation = operations[T][i].split(" ");
+
+                switch(operation[0]){
+                    case "I":
+                        if(maxHeapFlag) maxHeap.add(Integer.parseInt(operation[1]));
+                        else minHeap.add(Integer.parseInt(operation[1]));
+                        break;
+                    case "D":
+                        if(operation[1].equals("1")){
+                            if(maxHeapFlag) maxHeap.poll();
+                            else{
+                                maxHeap.addAll(minHeap);
+                                maxHeap.poll();
+                                minHeap = new PriorityQueue<>();
+                                maxHeapFlag = true;
+                            }
+                        } else{
+                            if(maxHeapFlag){
+                                minHeap.addAll(maxHeap);
+                                minHeap.poll();
+                                maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+                                maxHeapFlag = false;
+                            } else{
+                                minHeap.poll();
+                            }
+                        }
+                        break;
+                }
+            }
+
+            int max, min;
+
+            if(maxHeap.isEmpty() && minHeap.isEmpty())
+                stb.append("EMPTY").append("\n");
+            else {
+                if(maxHeapFlag){
+                    max = maxHeap.poll();
+                    if(maxHeap.isEmpty()) min = max;
+                    else{
+                        minHeap.addAll(maxHeap);
+                        min = minHeap.poll();
+                    }
+                }else{
+                    min = minHeap.poll();
+                    if(minHeap.isEmpty()) max = min;
+                    else{
+                        maxHeap.addAll(minHeap);
+                        max = maxHeap.poll();
+                    }
+                }
+                stb.append(max).append(" ").append(min);
+            }
+        }
+
+        return stb.toString();
+    }
+
+
+}
+class MainA7662{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        int[] operationCounts = new int[T];
+        String[][] operations = new String[T][];
+        while (--T >= 0) {
+            operationCounts[T] = Integer.parseInt(br.readLine());
+            operations[T] = new String[operationCounts[T]];
+
+            for(int i = 0; i < operationCounts[T]; i++) operations[T][i] = br.readLine();
+        }
+
+        System.out.print(new 이중우선순위큐_7662(operationCounts, operations).solution());
+    }
+}
