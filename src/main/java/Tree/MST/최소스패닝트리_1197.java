29509f7dea3aa5fb5d745003cb632528d1c20b15
JiheeSeon <starlover27@naver.com>
2021-07-15 19:13:35 +0900

    MST / 1197 최소 스패닝 트리 / prim

--- src/main/java/Tree/MST/최소스패닝트리_1197.java
+++ src/main/java/Tree/MST/최소스패닝트리_1197.java
@@ -1,9 +1,25 @@
 package Tree.MST;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
+
+/*
+TC
+7 11
+1 2 2
+2 3 5
+7 2 7
+7 3 2
+6 7 9
+3 6 3
+3 5 6
+3 1 20
+1 4 10
+4 5 1
+5 6 23
+19
+*/
 
 class 최소스패닝트리_1197 {
     int V, E;
@@ -53,6 +69,41 @@ class 최소스패닝트리_1197 {
 
     long solution_prim(){
         long ans = 0;
+        ArrayList<EdgeA1197>[] graph = new ArrayList[V + 1];
+        int startV = -1;
+
+        // setting graph
+        for(int v = 1; v <= V; v++)
+            graph[v] = new ArrayList<>();
+
+        for (int[] info : input) {
+            if(startV == -1) startV = info[0];
+            graph[info[0]].add(new EdgeA1197(info[0], info[1], info[2]));
+            graph[info[1]].add(new EdgeA1197(info[1], info[0], info[2]));
+        }
+        boolean[] hasParent = new boolean[V + 1];
+
+        // setting other variables
+        Set<Integer> selectedV = new HashSet<>();
+        PriorityQueue<EdgeA1197> edgePQ = new PriorityQueue<>();
+        edgePQ.addAll(graph[startV]);
+
+        EdgeA1197 selectedEdge;
+
+        while(!edgePQ.isEmpty() && selectedV.size() < E){
+            // 선택된 정점과 연결되는 간선 중 가장 가중치가 작은 것 추가
+            selectedEdge = edgePQ.poll();
+
+            // 이미 선택된 정점과 연결되어 있는 경우
+            if(selectedV.contains(selectedEdge.v2)) continue;
+            // 트리의 조건 : 부모는 무조건 하나
+            if(hasParent[selectedEdge.v2]) continue;
+
+            ans += selectedEdge.w;
+            selectedV.add(selectedEdge.v1);
+            hasParent[selectedEdge.v2] = true;
+            edgePQ.addAll(graph[selectedEdge.v2]);
+        }
 
         return ans;
     }
@@ -74,6 +125,7 @@ class EdgeA1197 implements Comparable<EdgeA1197>{
     }
 }
 
+
 class MainA1197{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -85,9 +137,10 @@ class MainA1197{
         for(int e = 0; e < E; e++)
             input[e] = strToIntArr(br.readLine());
 
-        System.out.println(new 최소스패닝트리_1197(V, E, input).solution_krukal());
+        System.out.println(new 최소스패닝트리_1197(V, E, input).solution_prim());
     }
     static int[] strToIntArr(String s){
         return Pattern.compile(" ").splitAsStream(s).mapToInt(Integer::parseInt).toArray();
     }
 }
+
