c570615bc7b1e37d27dc2ad6aa8ff0029ebfe683
JiheeSeon <starlover27@naver.com>
2021-07-22 13:35:35 +0900

    MST / 1647 도시분할계획 / kruskal fix, prim added

--- src/main/java/Tree/MST/도시분할계획_1647.java
+++ src/main/java/Tree/MST/도시분할계획_1647.java
@@ -3,8 +3,7 @@ package Tree.MST;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -25,7 +24,7 @@ class 도시분할계획_1647_Kruskal {
         int[] parent = IntStream.rangeClosed(0, V).toArray();
         PriorityQueue<EdgeA1647_Krukal> pq = Arrays.stream(edges).collect(Collectors.toCollection(PriorityQueue::new));
 
-        while(ptr < V && !pq.isEmpty()){
+        while(ptr < V - 1 && !pq.isEmpty()){
             now = pq.poll();
             if(!union(now.v1, now.v2, parent)) continue;
 
@@ -69,17 +68,88 @@ class EdgeA1647_Krukal implements Comparable<EdgeA1647_Krukal>{
     }
 }
 
+class 도시분할계획_1647_Prim {
+    int V;
+    ArrayList<EdgeA1647_Prim> [] graph;
+
+    public 도시분할계획_1647_Prim(int v, ArrayList<EdgeA1647_Prim>[] graph) {
+        V = v;
+        this.graph = graph;
+    }
+
+    int solve() {
+        int max = -1;
+        int ans = 0;
+
+        Set<Integer> visitedVSet = new HashSet<>();
+        visitedVSet.add(1);
+
+        PriorityQueue<EdgeA1647_Prim> pq = new PriorityQueue<>();
+        pq.addAll(graph[1]);
+
+        EdgeA1647_Prim now;
+        while (!pq.isEmpty() && visitedVSet.size() < V){
+            now = pq.poll();
+            if(visitedVSet.contains(now.v)) continue;
+
+            visitedVSet.add(now.v);
+            pq.addAll(graph[now.v]);
+            if(now.w > max) max = now.w;
+            ans += now.w;
+        }
+
+        return ans - max;
+    }
+}
+
+class EdgeA1647_Prim implements Comparable<EdgeA1647_Prim> {
+    int v, w;
+
+    public EdgeA1647_Prim(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
+
+    @Override
+    public int compareTo(EdgeA1647_Prim o) {
+        return Integer.compare(w, o.w);
+    }
+}
+
 class MainA1647{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] tmp;
+    static int V, E;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] tmp = strToIntArr(br.readLine());
-        int V = tmp[0]; int E = tmp[1];
+        tmp = strToIntArr(br.readLine());
+        V = tmp[0]; E = tmp[1];
+        runWithKrukal();
+
+        runWithPrim();
+    }
+
+    static void runWithPrim() throws IOException{
+        ArrayList<EdgeA1647_Prim>[] graph = new ArrayList[V + 1];
+        for(int v = 1; v <= V; v++)
+            graph[v] = new ArrayList<>();
+
+        for(int e = 0; e < E; e++){
+            tmp = strToIntArr(br.readLine());
+            graph[tmp[0]].add(new EdgeA1647_Prim(tmp[1], tmp[2]));
+            graph[tmp[1]].add(new EdgeA1647_Prim(tmp[0], tmp[2]));
+        }
+        System.out.println(new 도시분할계획_1647_Prim(V, graph).solve());
+    }
+
+    static void runWithKrukal() throws IOException{
         EdgeA1647_Krukal[] edges = new EdgeA1647_Krukal[E];
         for(int e = 0; e < E; e++){
             edges[e] = new EdgeA1647_Krukal(strToIntArr(br.readLine()));
         }
         System.out.println(new 도시분할계획_1647_Kruskal(V, edges).solve());
     }
+
     static int[] strToIntArr(String s){
         return Pattern.compile(" ").splitAsStream(s).mapToInt(Integer::parseInt).toArray();
     }
