e4a5b52e82403826311b3157ce5a4b2a315217cf
JiheeSeon <starlover27@naver.com>
2021-07-25 17:25:01 +0900

    MST / 1944 복제로봇 / wrong sol with bfs, 왜...?

--- src/main/java/Tree/MST/복제로봇_1944.java
+++ src/main/java/Tree/MST/복제로봇_1944.java
@@ -6,55 +6,97 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.IntStream;
 
 class 복제로봇_1944 {
     int N, K;
     char[][] maze;
     int[][] check;
+    int[] parent;
 
-    PointA1944 startCoordinate;
-    PriorityQueue<PointA1944> pq;
+    Point startCoordinate;
+    LinkedList<Point> keyLocations;
 
     final int[] dy = {-1, 1, 0, 0};
     final int[] dx = {0, 0, -1, 1};
 
-    public 복제로봇_1944(int n, int k, char[][] maze, PointA1944 startCoordinate) {
+
+    public 복제로봇_1944(int n, int k, char[][] maze, Point startCoordinate, LinkedList<Point> keyLocations) {
         N = n;
         K = k;
         this.maze = maze;
-        check = new int[N][N];
         this.startCoordinate = startCoordinate;
+        this.keyLocations = keyLocations;
+        parent = IntStream.rangeClosed(0, K).toArray();
     }
 
     int solve() {
-        pq = new PriorityQueue<>();
-        pq.add(new PointA1944(startCoordinate.y, startCoordinate.x, 0));
-        int ans = 0;
+        PriorityQueue<KruskalEdgeIW> pq = new PriorityQueue<>();
+        keyLocations.addFirst(startCoordinate);
 
-        PointA1944 p;
-        Set<PointA1944> set = new HashSet<>();
-        while (set.size() < K + 1) {
+        for (int idx = 0; idx < keyLocations.size() - 1; idx++) {
+            check = new int[N][N];
+            check[keyLocations.get(idx).y][keyLocations.get(idx).x] = 1;
+
+            bfs(keyLocations.get(idx));
+
+            for (int other = idx + 1; other < keyLocations.size(); other++) {
+                pq.add(new KruskalEdgeIW(idx, other, check[keyLocations.get(other).y][keyLocations.get(other).x] - 1));
+            }
+        }
+
+        int cnt = 0;
+        int ans = 0;
+        KruskalEdgeIW e;
+
+        while (cnt < K) {
             if(pq.isEmpty()) return -1;
 
-            p = pq.poll();
-            if(set.contains(p)) continue;
+            e = pq.poll();
 
-            dfs(p.y, p.x, p.w);
-            set.add(p);
-            ans += p.w;
+            if(!union(e.v1, e.v2)) continue;
+
+            cnt++;
+            ans += e.w;
         }
 
         return ans;
     }
 
-    void dfs(int y, int x, int w){
-        if(y < 0 || x < 0 || y >= N || x >= N || maze[y][x] == '1' || (check[y][x] != 0 && check[y][x] < w)) return;
+    boolean union(int a, int b){
+        int pA = find(a);
+        int pB = find(b);
 
-        check[y][x] = w;
-        if(maze[y][x] == 'K') pq.add(new PointA1944(y, x, w));
+        if(pA == pB) return false;
 
-        for(int i = 0; i < 4; i++){
-            dfs(y + dy[i], x + dx[i], w + 1);
+        if(pA < pB) parent[pB] = pA;
+        else parent[pA] = pB;
+
+        return true;
+    }
+    int find(int a) {
+        return parent[a] == a ? a : (parent[a] = find(parent[a]));
+    }
+
+    void bfs(Point a) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(a);
+
+        Point now;
+        int nextY, nextX;
+        while (!q.isEmpty()) {
+            now = q.poll();
+
+            for(int i = 0; i < 4; i++){
+                nextY = now.y + dy[i];
+                nextX = now.x + dx[i];
+
+                if(nextY < 0 || nextX < 0 || nextY >= N || nextX >= N
+                        || check[nextY][nextX] != 0 || maze[nextY][nextX] == '1') continue;
+
+                check[nextY][nextX] = check[now.y][now.x] + 1;
+                q.add(new Point(nextY, nextX));
+            }
         }
     }
 
@@ -68,57 +110,23 @@ class 복제로봇_1944 {
     }
 }
 
-class PointA1944 implements Comparable<PointA1944>{
-    int y, x, w;
-
-    public PointA1944(int y, int x, int w) {
-        this.y = y;
-        this.x = x;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(PointA1944 o) {
-        return Integer.compare(w, o.w);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PointA1944 that = (PointA1944) o;
-        return y == that.y && x == that.x && w == that.w;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(y, x, w);
-    }
-
-    @Override
-    public String toString() {
-        return "PointA1944{" +
-                "y=" + y +
-                ", x=" + x +
-                ", w=" + w +
-                '}';
-    }
-}
-
 class MainA1944 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] tmp = InputProcessor.strToIntArr(br.readLine());
         int N = tmp[0]; int K = tmp[1];
 
-        PointA1944 startCoordinate = null;
+        Point startCoordinate = null;
+        LinkedList<Point> keyLocations = new LinkedList<>();
         char[][] maze = new char[N][N];
         for(int y = 0; y < N; y++){
             maze[y] = br.readLine().toCharArray();
-            for(int x = 0; x < N; x++)
-                if(maze[y][x] == 'S') startCoordinate = new PointA1944(y, x, 0);
+            for(int x = 0; x < N; x++) {
+                if (maze[y][x] == 'S') startCoordinate = new Point(y, x);
+                if (maze[y][x] == 'K') keyLocations.add(new Point(y, x));
+            }
         }
 
-        System.out.println(new 복제로봇_1944(N, K, maze, startCoordinate).solve());
+        System.out.println(new 복제로봇_1944(N, K, maze, startCoordinate, keyLocations).solve());
     }
 }
