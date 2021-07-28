eae490408e44511fe6260529aa36f4d9467e7a36
JiheeSeon <starlover27@naver.com>
2021-07-28 20:24:55 +0900

    Tree / 15900 나무탈출 / time over - why...

---
+++ src/main/java/Tree/Basic/나무탈출_15900.java
@@ -0,0 +1,92 @@
+package Tree.Basic;
+
+import Util.InputProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+class 나무탈출_15900 {
+    int V;
+    ArrayList<Integer>[] graph;
+    boolean[] check;
+    int ans = 0;
+
+    public 나무탈출_15900(int v, ArrayList<Integer>[] graph) {
+        V = v;
+        this.graph = graph;
+        check = new boolean[V + 1];
+    }
+
+    // 리프노드로부터의 높이들의 합이 홀수인지
+    String solve() {
+        topdownDfs(1, 0);
+        return ans % 2 == 1 ? "Yes" : "No";
+    }
+
+    void topdownDfs(int now, int cnt) {
+        check[now] = true;
+
+        int leafChecker = 0;
+        for (int next : graph[now]) {
+            if(check[next]) continue;
+
+            leafChecker++;
+            topdownDfs(next,cnt + 1);
+        }
+
+        if(leafChecker == 0) ans += cnt;
+    }
+}
+
+class MainA15900{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 게임말이 게임판에 존재하지 않아 고를 수 없는 사람이 지게 된다.
+        int V = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer>[] graph = new ArrayList[V + 1];
+        for (int v = 1; v <= V; v++) graph[v] = new ArrayList<>();
+
+        int[] tmp;
+        for (int e = 0; e < V - 1; e++) {
+            tmp = InputProcessor.strToIntArr(br.readLine());
+            graph[tmp[0]].add(tmp[1]);
+            graph[tmp[1]].add(tmp[0]);
+        }
+
+        System.out.println(new 나무탈출_15900(V, graph).solve());
+    }
+}
+/*
+8
+8 1
+1 4
+7 4
+6 4
+6 5
+1 3
+2 3
+No ()
+
+19
+1 8
+3 5
+14 9
+13 9
+12 3
+8 15
+1 4
+19 18
+6 10
+11 6
+6 2
+9 17
+7 4
+16 8
+5 9
+17 18
+6 4
+3 1
+*/
