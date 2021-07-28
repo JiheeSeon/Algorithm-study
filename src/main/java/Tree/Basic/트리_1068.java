510d9363120cea295775955e26dffcbb90dfd3f5
JiheeSeon <starlover27@naver.com>
2021-07-28 15:35:32 +0900

    Tree / 1068 트리 / 트리의 원소 삭제

---
+++ src/main/java/Tree/Basic/트리_1068.java
@@ -0,0 +1,76 @@
+package Tree.Basic;
+
+import Util.InputProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class 트리_1068 {
+    int N, D;
+    int[] parent;
+    int root;
+    ArrayList<Integer>[] topDownGraph;
+    Set<Integer> leafSet;
+
+    public 트리_1068(int n, int d, int[] parent) {
+        N = n;
+        D = d;
+        this.parent = parent;
+    }
+
+    int solve() {
+        setTopDownGraph();
+        if(root == D) return 0;
+
+        // set leaf
+        leafSet = IntStream.range(0, N).boxed().collect(Collectors.toCollection(HashSet::new));
+        for(int idx = 0; idx < N; idx++) leafSet.remove(parent[idx]);
+        dfs(0, false);
+        return leafSet.size();
+    }
+
+    void setTopDownGraph() {
+        topDownGraph = new ArrayList[N];
+        for(int i = 0; i < N; i++) topDownGraph[i] = new ArrayList<>();
+
+        root = -1;
+        for(int idx = 0; idx < N; idx++){
+            if(parent[idx] == -1){
+                root = idx;
+                continue;
+            }
+            topDownGraph[parent[idx]].add(idx);
+        }
+    }
+
+    void dfs(int now, boolean deleteFlag) {
+        if(now == D) deleteFlag = true;
+        if(deleteFlag) leafSet.remove(now);
+
+        if(topDownGraph[now].size() == 0){
+            if(topDownGraph[parent[D]].size() == 1) leafSet.add(parent[D]);
+            return;
+        }
+
+        for (int child : topDownGraph[now]) {
+            dfs(child, deleteFlag);
+        }
+    }
+}
+
+class MainA1068{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] parent = InputProcessor.strToIntArr(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+
+        System.out.println(new 트리_1068(N, D, parent).solve());
+    }
+}
