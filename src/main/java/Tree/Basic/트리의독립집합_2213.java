222046f61aa1b990434581011770eeeb539600e7
Jihee <starlover27@naver.com>
2021-08-05 23:42:21 +0900

    Tree / 2213 트리의 독립집합 / 기본 sum dp로 저장하는 데까지, 셋 요소 저장하는 방법 추가 모색해야

--- src/main/java/Tree/Basic/트리의독립집합_2213.java
+++ src/main/java/Tree/Basic/트리의독립집합_2213.java
@@ -7,95 +7,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
 
-class 트리의독립집합_2213 {
+class 트리의독립집합_2213{
     int V;
     int[] weights;
     ArrayList<Integer>[] tree;
-    int[][] dp;
 
-    final static int IN_SET = 1;
-    final static int NOT_IN_SET = 0;
-    final static int DEFAULT = -1;
+    int[][] dp;
+    final int IN_SET = 1;
+    final int NOT_IN_SET = 0;
+    final int DEFAULT = -1;
 
     public 트리의독립집합_2213(int V, int[] weights, ArrayList<Integer>[] graph) {
         this.V = V;
         this.weights = weights;
 
         tree = new ArrayList[V + 1];
-        for (int v = 1; v <= V; v++)
-            tree[v] = new ArrayList<>();
+        for (int v = 1; v <= V; v++) tree[v] = new ArrayList<>();
+        setTree(1, new boolean[V + 1], graph);
 
-        makeTree(1, new boolean[V + 1], graph);
+        dp = new int[V + 1][2];
+        for (int v = 0; v <= V; v++) {
+            Arrays.fill(dp[v], DEFAULT);
+        }
     }
 
-    void makeTree(int node, boolean[] check, ArrayList<Integer>[] graph){
+    void setTree(int node, boolean[] check, ArrayList<Integer>[] graph) {
         check[node] = true;
 
         for (int child : graph[node]) {
             if(check[child]) continue;
 
             tree[node].add(child);
-            makeTree(child, check, graph);
+            setTree(child, check, graph);
         }
     }
 
-    String solve() {
-        dp = new int[V + 1][2];
-        for (int v = 0; v < V + 1; v++) Arrays.fill(dp[v], DEFAULT);
-
-        StringBuilder stb = new StringBuilder();
-
-        Set<Integer> setForElement = new TreeSet<>();
-        Set<Integer> setForNonElement = new TreeSet<>();
-
-        int retInSet = solve(1, IN_SET, setForElement);
-        int retNotInSet = solve(1, NOT_IN_SET, setForNonElement);
-
-        if(retInSet > retNotInSet){
-            setForElement.add(1);
-            stb.append(retInSet).append("\n");
-            setForElement.forEach(s -> stb.append(s).append(" "));
-        }else{
-            stb.append(retNotInSet).append("\n");
-            setForNonElement.forEach(s -> stb.append(s).append(" "));
-        }
-
-        return stb.toString();
+    int solve() {
+        return Math.max(solve(1, IN_SET), solve(1, NOT_IN_SET));
     }
 
-    int solve(int root, int elementFlag, Set<Integer> set) {
-        if(tree[root].isEmpty()) {
-            return elementFlag == IN_SET ? weights[root] : 0;
+    private int solve(int node, int setFeature) {
+        if(dp[node][setFeature] != -1) return dp[node][setFeature];
+
+        if(setFeature == IN_SET){
+            dp[node][setFeature] = weights[node];
+
+            for (int child : tree[node])
+                dp[node][setFeature] += solve(child, NOT_IN_SET);
+        } else{
+            dp[node][setFeature] = 0;
+
+            for (int child : tree[node])
+                dp[node][setFeature] += Math.max(solve(child, NOT_IN_SET), solve(child, IN_SET));
         }
-
-        if(dp[root][elementFlag] != DEFAULT) return dp[root][elementFlag];
-
-        dp[root][elementFlag] = elementFlag == IN_SET ? weights[root] : 0;
-
-        if (elementFlag == IN_SET) {
-            for (int child : tree[root]) {
-                dp[root][elementFlag] += solve(child, NOT_IN_SET, set);
-            }
-            return dp[root][elementFlag];
-        }else {
-            for (int child : tree[root]) {
-                int retInSet = solve(child, IN_SET, set);
-                int retNotInSet = solve(child, NOT_IN_SET, set);
-
-                if (retInSet > retNotInSet) {
-                    set.add(child);
-                    dp[root][elementFlag] += retInSet;
-                } else {
-                    set.remove(child);
-                    dp[root][elementFlag] += retNotInSet;
-                }
-            }
-
-            return dp[root][elementFlag];
-        }
+        return dp[node][setFeature];
     }
 }
 
