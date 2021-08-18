6a3c69f8901523808aad5357f02eccb23ac6d7d6
Jihee <starlover27@naver.com>
2021-08-18 15:22:38 +0900

    Tree / 11812 K진트리 / 13% timeout still. applied cache for result & considered case of 1

--- src/main/java/Tree/Basic/K진트리_11812.java
+++ src/main/java/Tree/Basic/K진트리_11812.java
@@ -12,22 +12,32 @@ class K진트리_11812 {
     long N, K;
     HashMap<Long, Long>[] parent;
     final int LGN;
+    HashMap<Pair, Integer> result;
 
     public K진트리_11812(long n, long k) {
         N = n;
         K = k;
         LGN = (int)(Math.log10(getLevel(N)) / Math.log10(2)) + 1;
 
-        parent = new HashMap[LGN];
-        for (int i = 0; i < parent.length; i++) parent[i] = new HashMap<>();
-        for (HashMap<Long, Long> longLongHashMap : parent) longLongHashMap.put(1L, -1L);
+        if(K != 1) {
+            parent = new HashMap[LGN];
+            for (int i = 0; i < parent.length; i++) parent[i] = new HashMap<>();
+            for (HashMap<Long, Long> longLongHashMap : parent) longLongHashMap.put(1L, -1L);
+        }
+
+        result = new HashMap<>();
     }
 
     private int getLevel(long a) {
         return (int)Math.ceil(Math.log10(a * (K - 1) + 1) / Math.log10(K)) - 1;
     }
 
-    int solve(long a, long b) {
+    long solve(long a, long b) {
+        if(K == 1) return b > a ? b - a : a - b;
+
+        Pair pair = new Pair(a, b);
+        if(result.containsKey(pair)) return result.get(pair);
+
         int levelA = getLevel(a);
         int levelB = getLevel(b);
 
@@ -63,7 +73,10 @@ class K진트리_11812 {
             }
         }
 
-        if(a == ancestorB) return levelB - levelA;
+        if(a == ancestorB){
+            if(!result.containsKey(pair)) result.put(pair, levelB - levelA);
+            return result.get(pair);
+        }
 
 
         // b와 a의 공통조상 찾기
@@ -75,7 +88,8 @@ class K진트리_11812 {
             ancestorB = parent[(int) digit].get(ancestorB);
         }
 
-        return getLevel(a) + 1 - getLevel(ancestorA) + getLevel(b) + 1 - getLevel(ancestorB);
+        if(!result.containsKey(pair)) result.put(pair, getLevel(a) + 1 - getLevel(ancestorA) + getLevel(b) + 1 - getLevel(ancestorB));
+        return result.get(pair);
     }
 
     private void bottomUpDfs(long now, ArrayList<Long> visitedNodes) {
@@ -93,6 +107,28 @@ class K진트리_11812 {
             for (long node : nodes) {
                 parent[i].put(node, parent[i - 1].get(node) == -1 ? -1 : parent[i - 1].get(parent[i - 1].get(node)));
             }
+        }
+    }
+
+    private class Pair{
+        long a, b;
+
+        public Pair(long a, long b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return a == pair.a && b == pair.b;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(a, b);
         }
     }
 }
