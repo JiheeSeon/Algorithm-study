85a1f851a4f3191c351ca20430f78450641ef907
Jihee <starlover27@naver.com>
2021-08-20 14:35:25 +0900

    Tree / 14267 회사문화 / changed to bottom up approach, wrong sol

--- src/main/java/Tree/Basic/회사문화_14267.java
+++ src/main/java/Tree/Basic/회사문화_14267.java
@@ -5,28 +5,37 @@ import Util.InputProcessor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class 회사문화_14267 {
-    int V;
-    ArrayList<Integer>[] graph;
+    final int V;
+    final int ROOT;
+
+    int[] parent;
     int[][] praises;
     int[] result;
+    Set<Integer> leafSet;
     Map<Integer, Integer> praiseMap;
 
-    public 회사문화_14267(int v, ArrayList<Integer>[] graph, int[][] praises) {
+    public 회사문화_14267(int v, int ROOT, int[] parent, Set<Integer> leafSet, int[][] praises) {
         V = v;
-        this.graph = graph;
+        this.ROOT = ROOT;
+
+        this.parent = parent;
+        this.leafSet = leafSet;
         this.praises = praises;
+
         result = new int[V + 1];
         praiseMap = new HashMap<>();
     }
 
     String solve() {
         for (int[] praise : praises)
-            praiseMap.put(praise[0], praiseMap.getOrDefault(praise[1], 0));
+            praiseMap.put(praise[0], praiseMap.getOrDefault(praise[0], 0) + praise[1]);
+
+        for(int leaf : leafSet) bottomUpDfs(leaf);
 
         StringBuilder stb = new StringBuilder();
         for (int i = 1; i <= V; i++) {
@@ -35,12 +44,13 @@ class 회사문화_14267 {
         return stb.toString();
     }
 
-    void dfs(int node, int praisePoint) {
-        result[node] += praisePoint;
+    int bottomUpDfs(int node) {
+        if(node == ROOT) return 0;
 
-        for (int child : graph[node]) {
-            dfs(child, praisePoint);
-        }
+        int res = bottomUpDfs(parent[node]);
+        if(praiseMap.containsKey(node)) res += praiseMap.get(node);
+        result[node] += res;
+        return res;
     }
 }
 
@@ -50,18 +60,18 @@ class MainA14267{
         int[] tmp = InputProcessor.strToIntArr(br.readLine());
         int V = tmp[0]; int praiseN = tmp[1];
 
-        tmp = InputProcessor.strToIntArr(br.readLine());
-        ArrayList<Integer>[] graph = new ArrayList[V + 1];
-        for (int v = 1; v <= V; v++) graph[v] = new ArrayList<>();
-        for (int v = 1; v <= V; v++) {
-            if(tmp[v - 1] == -1) continue;
-            graph[tmp[v - 1]].add(v);
+        Set<Integer> leafSet = IntStream.rangeClosed(1, V).boxed().collect(Collectors.toCollection(TreeSet::new));
+        int root = 0;
+        int[] parent = InputProcessor.strToIntArr("0 " + br.readLine());
+        for (int i : parent) {
+            leafSet.remove(i);
+            if(i == -1) root = i;
         }
 
         int[][] praises = new int[praiseN][2];
         for (int p = 0; p < praiseN; p++)
             praises[p] = InputProcessor.strToIntArr(br.readLine());
 
-        System.out.println(new 회사문화_14267(V, graph, praises).solve());
+        System.out.println(new 회사문화_14267(V, root, parent, leafSet, praises).solve());
     }
 }
