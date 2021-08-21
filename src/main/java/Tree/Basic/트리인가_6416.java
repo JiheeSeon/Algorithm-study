cef30bbd5a1c080a7b195a4f67c213be60e1abdf
Jihee <starlover27@naver.com>
2021-08-21 14:50:40 +0900

    Tree / 트리인가 6416 / 아직도..

--- src/main/java/Tree/Basic/트리인가_6416.java
+++ src/main/java/Tree/Basic/트리인가_6416.java
@@ -8,50 +8,56 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class 트리인가_6416 {
-    int E;
+    int V, E;
+    Set<Integer> vertices;
     ArrayList<int[]> edges;
     Map<Integer, ArrayList<Integer>> graph;
 
-    public 트리인가_6416(ArrayList<int[]> edges) {
-        E = edges.size();
+    public 트리인가_6416(Set<Integer> vertices, ArrayList<int[]> edges, Map<Integer, ArrayList<Integer>> graph) {
+        this.vertices = vertices;
         this.edges = edges;
+
+        V = vertices.size(); E = edges.size();
+        this.graph = graph;
     }
 
     boolean solve() {
-        graph = new HashMap<>();
-        ArrayList<Integer> valList;
+        if(vertices.isEmpty() && edges.isEmpty()) return true;
 
-        Set<Integer> in = new HashSet<>();
-        Set<Integer> out = new HashSet<>();
+        if(V - 1 != E) return false;
 
+        Map<Integer, Integer> checkMap = new HashMap<>();
         for (int[] edge : edges) {
-            valList = graph.getOrDefault(edge[0], new ArrayList<>());
-            valList.add(edge[1]);
-            graph.put(edge[0], valList);
+            if(edge[0] == edge[1]) return false;
+            checkMap.put(edge[1], checkMap.getOrDefault(edge[1], 0) + 1);
+        }
+        System.out.println(checkMap);
 
-            out.add(edge[0]);
-            in.add(edge[1]);
+        Set<Integer> rootCandidates = graph.keySet();
+        rootCandidates.removeAll(checkMap.keySet());
+
+        if(rootCandidates.size() != 1) return false;
+
+        for (int c : checkMap.keySet()) {
+            if(checkMap.get(c) >= 2) return false;
         }
 
-        out.removeAll(in);
-
-        if(out.size() != 1 || in.size() != E) return false;
-
-        int root = 0;
-        for(int i : out) root = i;
-
-        in.add(root);
-        dfs(root, in);
-
-        return in.size() == 0;
+        int root = -1;
+        for (int i : rootCandidates) {
+            root = i;
+        }
+        return dfs(root, new boolean[V + 1]);
     }
 
-    void dfs(int node, Set<Integer> unvisitedSet) {
-        if(!unvisitedSet.contains(node)) return;
+    boolean dfs(int node, boolean[] check) {
+        if(check[node]) return false;
 
-        unvisitedSet.remove(node); // check[node] = true;
-        ArrayList<Integer> children = graph.getOrDefault(node, new ArrayList<>());
-        for(int next : children) dfs(next, unvisitedSet);
+        check[node] = true;
+        boolean ret = true;
+        for (int child : graph.get(node)){
+            if(!dfs(child, check)) ret = false;
+        }
+        return ret;
     }
 }
 
@@ -61,32 +67,54 @@ class MainA6416{
 
         int t = 0;
         String s;
+
+        Map<Integer, ArrayList<Integer>> graph;
         ArrayList<int[]> edges;
         boolean breakFlag = false;
         StringBuilder stb = new StringBuilder();
 
         String[] tmp;
         int[] edge;
+        ArrayList<Integer> nextVertices;
+        Set<Integer> vertices;
 
         while(true){
             edges = new ArrayList<>();
+            graph = new HashMap<>();
+            vertices = new HashSet<>();
 
             LL:
             while((s = br.readLine()).length() > 0) {
                 tmp = s.split("  ");
                 for (String str : tmp){
                     edge = InputProcessor.strToIntArr(str);
-                    if(!(edge[0] <= 0 && edge[1] <= 0)) edges.add(edge);
+                    if(!(edge[0] <= 0 && edge[1] <= 0)){
+                        edges.add(edge);
+
+                        nextVertices = graph.getOrDefault(edge[0], new ArrayList<>());
+                        graph.put(edge[0], nextVertices);
+
+                        vertices.add(edge[0]); vertices.add(edge[1]);
+                    }
                     if(edge[0] < 0 && edge[1] < 0){
                         breakFlag = true; break LL;
                     }
                 }
             }
 
-            stb.append("Case ").append(++t).append(" is ").append(new 트리인가_6416(edges).solve() ? "" : "not ").append("a tree.\n");
+            stb.append("Case ").append(++t).append(" is ").append(new 트리인가_6416(vertices, edges, graph).solve() ? "" : "not ").append("a tree.\n");
             if(breakFlag) break;
         }
 
         System.out.print(stb);
     }
 }
+/*
+1 2 0 0
+-1 -1
+Case 1 is a tree.
+
+1 2  2 1  0 0
+-1 -1
+Case 1 is not a tree.
+*/
