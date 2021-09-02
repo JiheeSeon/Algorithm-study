eae86403ad6bcbe664df31287c371ee43bdce54d
JiheeSeon <starlover27@naver.com>
2021-09-02 15:43:15 +0900

    기본 수학 / sum, max

--- src/main/java/Roadmap/Lv1/지능형기차2_2460.java
+++ src/main/java/Roadmap/Lv1/지능형기차2_2460.java
@@ -1,2 +1,24 @@
-package Roadmap.Lv1;public class 지능형기차2_2460 {
+package Roadmap.Lv1;
+
+import Util.InputProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class MainA2460{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] tmp;
+        int cnt = 0;
+        int max = -1;
+        for (int i = 0; i < 10; i++) {
+            tmp = InputProcessor.strToIntArr(br.readLine());
+            cnt -= tmp[0];
+            cnt += tmp[1];
+
+            if(max < cnt) max = cnt;
+        }
+        System.out.println(max);
+    }
 }
