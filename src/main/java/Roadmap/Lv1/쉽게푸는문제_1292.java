7d495ba298555d6dd24fcb926fc0c00dcf1479a0
JiheeSeon <starlover27@naver.com>
2021-09-02 19:30:02 +0900

    기본 / 1292 쉽게 푸는 문제 / 90% 시간초과

--- src/main/java/Roadmap/Lv1/쉽게푸는문제_1292.java
+++ src/main/java/Roadmap/Lv1/쉽게푸는문제_1292.java
@@ -5,40 +5,47 @@ import Util.InputProcessor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 class 쉽게푸는문제_1292 {
     static long solve(int start, int end) {
         //set ends arr
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= 1000; i++)
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int i = 1; i <= 45; i++)
             map.put(i*(i + 1) / 2, i);
+
+        int dist = end - start + 1;
 
         long sum = 0;
         int cnt;
         int startGroup, endGroup;
 
         // set start group
-        if(map.containsKey(start)){
-            startGroup = map.get(start);
-            sum += startGroup;
-        } else {
-            cnt = 0;
-            while(!map.containsKey(start++)){cnt++;}
-            startGroup = map.get(start - 1);
-            sum += ((long) (cnt + 1) * startGroup);
+        cnt = 0;
+        while(true){
+            cnt++;
+            if(map.containsKey(start)){
+                startGroup = map.get(start);
+                sum += (long)startGroup * cnt;
+                break;
+            }
+            start++;
         }
 
-        // set end group
-        if(map.containsKey(end)){
-            endGroup = map.get(end);
-            sum += ((long) endGroup * endGroup);
-        } else{
-            cnt = 0;
-            while(!map.containsKey(end--)){cnt++;}
-            endGroup = map.get(end + 1) + 1;
-            sum += ((long) cnt * endGroup);
+        cnt = 0;
+        while (true) {
+            --end; ++cnt;
+
+            if (map.containsKey(end)) {
+                endGroup = map.get(end) + 1;
+                sum += (long)endGroup * cnt;
+                break;
+            }
+        }
+
+        if (startGroup == endGroup) {
+            return (long) endGroup * dist;
         }
 
         // add between start & end group
