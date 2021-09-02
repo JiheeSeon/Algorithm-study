adbe86dbbc5b088d7072a0d1f7d697935202fd20
JiheeSeon <starlover27@naver.com>
2021-09-02 16:41:16 +0900

    기본 수학 / 피보나치수 - DP 이용

--- src/main/java/Roadmap/Lv1/피보나치수5_10870.java
+++ src/main/java/Roadmap/Lv1/피보나치수5_10870.java
@@ -3,22 +3,26 @@ package Roadmap.Lv1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Objects;
+
+class 피보나치수5_10870{
+    static BigInteger[] dp = new BigInteger[1000];
+
+    static BigInteger fibo(int N) {
+        if(N == 0) return BigInteger.ZERO;
+        else if(N == 1 || N == 2) return BigInteger.ONE;
+        else if(!Objects.equals(dp[N], null)) return dp[N];
+
+        dp[N] = fibo(N - 1).add(fibo(N - 2));
+        return dp[N];
+    }
+}
 
 class MainA10870{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        switch (N){
-            case 0 -> {System.out.println(0); return;}
-            case 1, 2 -> {System.out.println(1); return;}
-            default ->{
-                long[] fiboNumbers = new long[N + 1];
-                fiboNumbers[1] = 1; fiboNumbers[2] = 1;
-                for (int i = 3; i <= N; i++) {
-                    fiboNumbers[i] = fiboNumbers[i - 1] + fiboNumbers[i - 2];
-                }
-                System.out.println(fiboNumbers[N]);
-            }
-        }
+        System.out.println(피보나치수5_10870.fibo(N));
     }
 }
