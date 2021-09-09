9bc18bec8e6d697520057e70fb6906b14cd6b5d7
JiheeSeon <starlover27@naver.com>
2021-09-09 10:37:14 +0900

    Segment Tree Algorithm / update added

--- src/main/java/CramForCodingTest/SampleCode/SegmentTree.java
+++ src/main/java/CramForCodingTest/SampleCode/SegmentTree.java
@@ -5,57 +5,80 @@ import Util.InputProcessor;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/*
+구간합구하기 4, 1 문제 참고
+*/
 
 class SegmentTree {
     int N, H;
-    int[] input;
-    int[] tree;
+    BigInteger[] input;
+    BigInteger[] tree;
 
-    public SegmentTree(int N, int[] input) {
+    public SegmentTree(int N, BigInteger[] input) {
         this.N = N;
         this.input = input;
 
         // set basic information of tree
         H = (int) (Math.ceil(Math.log10(N) / Math.log10(2)));
-        tree = new int[1 << (H + 1)]; // 2^(H + 1)
+        tree = new BigInteger[1 << (H + 1)]; // 2^(H + 1)
 
         init(0, input.length - 1, 1);
     }
 
-    int init(int start, int end, int idx) {
+    BigInteger init(int start, int end, int idx) {
         if(start == end){
             tree[idx] = input[start];
             return tree[idx];
         }
 
         int mid = (start + end) / 2;
-        return (tree[idx] = init(start, mid, idx * 2) + init(mid + 1, end, idx * 2 + 1));
+        return (tree[idx] = init(start, mid, idx * 2).add(init(mid + 1, end, idx * 2 + 1)));
     }
 
-    int sum(int a, int b){
-        return sum(0, N - 1, a - 1, b - 1, 1);
+    BigInteger sum(int a, int b){
+        return sum(0, N - 1, a, b, 1);
     }
 
-    private int sum(int start, int end, int a, int b, int idx) {
-        if(end < a || start > b) return 0;
+    private BigInteger sum(int start, int end, int a, int b, int idx) {
+        if(end < a || start > b) return BigInteger.ZERO;
         else if(a <= start && end <= b) return tree[idx];
         int mid = (start + end) / 2;
-        return sum(start, mid, a, b, idx * 2) + sum(mid + 1, end, a, b, idx * 2 + 1); // 두 트리 모두 사용
+        return sum(start, mid, a, b, idx * 2).add(sum(mid + 1, end, a, b, idx * 2 + 1)); // 두 트리 모두 사용
+    }
+
+    void update(long idxToUpdate, long valToUpdate){
+        update((int)idxToUpdate, valToUpdate, 0, N - 1, 1);
+    }
+
+    private BigInteger update(int idxToUpdate, long valToUpdate, int start, int end, int treeIdx){
+        if(start == end) return (tree[treeIdx] = BigInteger.valueOf(valToUpdate));
+
+        int mid = (start + end) / 2;
+        if(idxToUpdate <= mid) return (tree[treeIdx] = update(idxToUpdate, valToUpdate, start, mid, treeIdx * 2).add(tree[treeIdx * 2 + 1]));
+        else return (tree[treeIdx] = update(idxToUpdate, valToUpdate, mid + 1, end, treeIdx * 2 + 1).add(tree[treeIdx * 2]));
     }
 }
 
 class SegmentTreeApp{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] tmp = InputProcessor.strToIntArr(br.readLine());
-        int N = tmp[0]; int M = tmp[1];
-        int[] input = InputProcessor.strToIntArr(br.readLine());
+        int[] intTmp = InputProcessor.strToIntArr(br.readLine());
+        long[] longTmp;
+        int N = intTmp[0]; int loopN = intTmp[1] + intTmp[2];
+
+        BigInteger[] input = new BigInteger[N];
+        for(int i = 0; i < N; i++) input[i] = BigInteger.valueOf(Long.parseLong(br.readLine()));
+
         StringBuilder stb = new StringBuilder();
         SegmentTree segmentTree = new SegmentTree(N, input);
 
-        while (M-- > 0) {
-            tmp = InputProcessor.strToIntArr(br.readLine());
-            stb.append(segmentTree.sum(tmp[0], tmp[1])).append("\n");
+
+        while (loopN-- > 0) {
+            longTmp = InputProcessor.strToLongArr(br.readLine());
+            if(longTmp[0] == 1) segmentTree.update(longTmp[1] - 1, longTmp[2]);
+            else stb.append(segmentTree.sum((int)longTmp[1] - 1, (int)longTmp[2] - 1)).append("\n");
         }
 
         System.out.print(stb);
