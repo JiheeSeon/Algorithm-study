5a2a0dc64a62ae082e3315c40171fb15fbd1269c
JiheeSeon <starlover27@naver.com>
2021-09-10 12:50:46 +0900

    DP / 12015  가장 긴 증가하는 부분 수열

---
+++ src/main/java/CramForCodingTest/PS/가장긴증가하는부분수열_12015_DP.java
@@ -0,0 +1,46 @@
+package CramForCodingTest.PS;
+
+import Util.InputProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class 가장긴증가하는부분수열2_12015_DP {
+    int N;
+    int[] arr;
+
+    public 가장긴증가하는부분수열2_12015_DP(int n, int[] arr) {
+        N = n;
+        this.arr = arr;
+    }
+
+    int solve() {
+        int[] dp = new int[N]; // i번째 원소를 마지막 원소로 가지는 최장수열의 길이
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            if(dp[i] == 0) dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j]) {
+                    if (dp[i] < dp[j] + 1) dp[i] = dp[j] + 1;
+                }
+            }
+
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
+
+class MainA12015_DP{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = InputProcessor.strToIntArr(br.readLine());
+
+        System.out.println(new 가장긴증가하는부분수열2_12015_DP(N, arr).solve());
+    }
+}
