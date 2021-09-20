ecf045b84841e3aee6bb7769c20dd2331b8cfd6b
JiheeSeon <starlover27@naver.com>
2021-09-20 17:54:12 +0900

    구현 / 14503 로봇 청소기 / solving...ㅜ

---
+++ src/main/java/Implementation/Acmicpc/로봇청소기_14503.java
@@ -0,0 +1,173 @@
+package Implementation.Acmicpc;
+
+import Util.InputProcessor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+문제
+로봇 청소기가 주어졌을 때, 청소하는 영역의 개수를 구하는 프로그램을 작성하시오.
+
+로봇 청소기가 있는 장소는 N×M 크기의 직사각형으로 나타낼 수 있으며, 1×1크기의 정사각형 칸으로 나누어져 있다.
+각각의 칸은 벽 또는 빈 칸이다. 청소기는 바라보는 방향이 있으며, 이 방향은 동, 서, 남, 북중 하나이다.
+지도의 각 칸은 (r, c)로 나타낼 수 있고, r은 북쪽으로부터 떨어진 칸의 개수, c는 서쪽으로 부터 떨어진 칸의 개수이다.
+
+N -> yHeight (세로 길이)    M -> xWidth(가로 길이)
+r -> y                     c -> x
+
+로봇 청소기는 다음과 같이 작동한다.
+
+1. 현재 위치를 청소한다.
+2. 현재 위치에서 현재 방향을 기준으로 왼쪽 방향부터 차례대로 인접한 칸을 탐색한다.
+    a. 왼쪽 = -1 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
+    b. 왼쪽 = -1 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다. //for 문 내
+    c. 네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
+    d. 네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
+* 로봇 청소기는 이미 청소되어있는 칸을 또 청소하지 않으며, 벽을 통과할 수 없다.
+
+문제 분석
+int[][] visited = input으로 받아온 이중배열
+int[][] directions = {{-1, 0}, {1, 0}, {1, 0}, {-1, 0}} // 북 0 -> 동 1 -> 남 2 -> 서 3
+
+1. 현재 위치 청소 -> visited[y][x] = 2;
+2. 현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 탐색 -> 서쪽 = 4
+    a.왼쪽 = 시계 반대방향
+        - nextY = y + dy[currD - 1 < 0 ? currD - 1 + 4 : currD - 1]
+        - nextX = x + dx[currD - 1 < 0 ? currD - 1 + 4 : currD - 1]
+    b. c. d. 구조 파악
+*/
+
+class MainA14503{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] tmp = InputProcessor.strToIntArr(br.readLine());
+        final int yHeight = tmp[0];
+        final int xWidth = tmp[1];
+
+        final int UNCLEANED = 0;
+        final int WALL = 1;
+        final int CLEANED = 2;
+
+        int[] yxd = InputProcessor.strToIntArr(br.readLine());
+        int nowY = yxd[0]; int nowX = yxd[1]; int nowD = yxd[2];
+
+        int[][] deltaD = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 북 0 -> 동 1 -> 남 2 -> 서 3
+
+        int[][] visited = new int[yHeight][xWidth];
+        for (int y = 0; y < yHeight; y++)
+            visited[y] = InputProcessor.strToIntArr(br.readLine());
+
+        int[][] start = new int[yHeight][xWidth];
+        for (int y = 0; y < yHeight; y++) {
+            for (int x = 0; x < xWidth; x++) {
+                if(visited[y][x] == 1) start[y][x] = 5;
+                else start[y][x] = 1;
+            }
+        }
+
+        int cnt = 0;
+        int nextD, nextY, nextX;
+
+        boolean ISCLEANABLE = true;
+
+        while(true){
+            // 2번
+//            if(CLEANABLE && visited[nowY][nowX] == CLEANED) break;
+            System.out.println("phase 1. NOW (" + nowD + ", " + nowY + ", "+ nowX + ")" + " -> " + start[nowY][nowX] + ", " + (cnt + 1));
+            if(start[nowY][nowX] == 6) break;
+
+            for(int y = 0; y < yHeight; y++)
+                System.out.println(Arrays.toString(start[y]));
+
+            if(ISCLEANABLE && visited[nowY][nowX] == UNCLEANED){
+//                System.out.println("( + " + nowY + ", " + nowX + ")" + visited[nowY][nowX]);
+                visited[nowY][nowX] = CLEANED;
+                cnt++;
+            }
+
+            ISCLEANABLE = false;
+
+            for(int i = start[nowY][nowX]; i <= 4; i++){
+                nextD = nowD < i ? (nowD - i + 4) % 4 : nowD - i;
+                nextY = nowY + deltaD[nextD][0];
+                nextX = nowX + deltaD[nextD][1];
+
+                System.out.println("Inside for loop. (" + nextD + ", " + nextY + ", " + nextX + ")" + " -> " + (0 <= nextY && nextY < yHeight && 0 <= nextX && nextX < xWidth ? visited[nextY][nextX] : "Out of bounds"));
+
+                if(0 <= nextY && nextY < yHeight && 0 <= nextX && nextX < xWidth && visited[nextY][nextX] == UNCLEANED){
+                    start[nowY][nowX] = i + 1;
+
+                    nowD = nextD; nowY = nextY; nowX = nextX;
+                    System.out.println("phase 2. nextD = " + nowD + " nextY = " + nowY + " nextX = " + nowX);
+
+                    ISCLEANABLE = true;
+                    System.out.println();
+                    break;
+                }
+            }
+
+            if (!ISCLEANABLE) {
+                // 위에서 break가 안되었으면 == 어디든 청소 불가능했다는 의미
+                start[nowY][nowX] = 5;
+
+                nextD = nowD < 2 ? (nowD + 2) % 4 : nowD - 2;
+                nextY = nowY + deltaD[nextD][0];
+                nextX = nowX + deltaD[nextD][1];
+
+                if(nextY < 0 || nextX < 0 || nextY >= yHeight || nextX >= xWidth || visited[nextY][nextX] == WALL || start[nextY][nextX] == 6){
+                    start[nowY][nowX] = 6;
+                    break;
+                }
+
+                nowD = nextD; nowY = nextY; nowX = nextX;
+                ISCLEANABLE = false;
+                System.out.println("phase 2.1. nextD = " + nowD + " nextY = " + nowY + " nextX = " + nowX);
+                System.out.println();
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
+
+/*
+11 10
+7 4 0
+0  1 1 1 1 1 1 1 1 1 1
+1  1 0 0 0 0 0 0 0 0 1
+2  1 0 0 0 1 1 1 1 0 1
+3  1 0 0 1 1 0 0 0 0 1
+4  1 0 1 1 0 0 0 0 0 1
+5  1 0 0 0 0 0 0 0 0 1
+6  1 0 0 0 0 0 0 1 0 1
+7  1 0 0 0 0 0 1 1 0 1
+8  1 0 0 0 0 0 1 1 0 1
+9  1 0 0 0 0 0 0 0 0 1
+10 1 1 1 1 1 1 1 1 1 1
+
+0 1 2 3
+북동남서
+
+11 10
+7 4 0
+    0   1   2   3   4   5   6   7   8   9
+0  [1] [1] [1] [1] [1] [1] [1] [1] [1] [1]
+1  [1] [0] [0] [0] [0] [1] [1] [1] [0] [1]
+2  [1] [0] [0] [1] [1] [1] [1] [1] [0] [1]
+3  [1] [0] [0] [1] [1] [0] [0] [0] [0] [1]
+4  [1] [0] [1] [1] [0] [0] [0] [0] [0] [1]
+5  [1] [0] [0] (12)(11)[0] [0] [0] [0] [1]
+6  [1] [0] (14)(13)(10)(9) [0] [1] [0] [1]
+7  [1] (16)(15)(2) (1) (8) [1] [1] [0] [1]
+8  [1] (17)(18)(3) (4) (7) [1] [1] [0] [1]
+9  [1] (21)(19)(20)(5) (6) [0] [0] [0] [1]
+10 [1] [1] [1] [1] [1] [1] [1] [1] [1] [1]
+
+왜 21이 아니지?
+
+57
+*/
